cc7c45ec5919bc3feefb102a1752770598886a8d
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2009-01-30 20:09:06 +0000

    add thread-safety based on atomicity-policy
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@15 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes++/socket.hh
+++ trunk/codes++/socket.hh
@@ -18,159 +18,195 @@
 #include <err.h>
 
 #include <sockaddress.hh>
+#include <atomicity-policy.hh>
 #include <tr1/array>
 
 namespace more {
 
-    template <int FAMILY, bool LOG>
+#ifdef _REENTRANT
+    typedef atomicity::GNU_CXX  default_socket_atomicity_policy;    
+#else
+    typedef atomicity::NONE  default_socket_atomicity_policy;    
+#endif
+
+    template <int FAMILY, typename ATOMICITY>
     class base_socket  
     {
     public:
 
         int 
-        init(int type,int protocol=0) 
-        {
-            if (_M_fd != -1) {
-                std::clog  << "init: socket already open" << std::endl;
-                return -1;
-            }
-
-            _M_fd = ::socket(FAMILY, type, protocol);
-            if (_M_fd == -1) {
-                std::clog  << "init: " << strerror(errno) << std::endl;
-            }
-
-            return _M_fd  == -1 ? -1 : 0;
-        }
-
-        operator bool()
-        { return _M_fd  == -1 ? false : true; }
-
-        int 
-        send(const void *buf, size_t len, int flags) 
-        { return _log("send", ::send(_M_fd, buf, len, flags)); }
+        send(const void *buf, size_t len, int flags) const
+        { return ::send(_M_fd, buf, len, flags); }
 
         template <std::size_t N>
-        int send(const std::tr1::array<iovec,N> &iov, int flags)
+        int send(const std::tr1::array<iovec,N> &iov, int flags) const
         { 
             const msghdr msg = { NULL, 0, const_cast<iovec *>(&iov.front()), N, NULL, 0, 0 };    
-            return _log("sendmsg", ::sendmsg(_M_fd, &msg, flags)); 
+            return ::sendmsg(_M_fd, &msg, flags); 
         }
 
         int 
-        recv(void *buf, size_t len, int flags) 
-        { return _log("recv", ::recv(_M_fd, buf, len, flags)); }
+        recv(void *buf, size_t len, int flags) const
+        { return ::recv(_M_fd, buf, len, flags); }
 
         template <std::size_t N>
-        int recv(std::tr1::array<iovec,N> &iov, int flags)
+        int recv(std::tr1::array<iovec,N> &iov, int flags) const
         { 
             msghdr msg = { NULL, 0, &iov.front(), N, NULL, 0, 0 };    
-            return _log("recvmsg", ::recvmsg(_M_fd, &msg, flags)); 
+            return ::recvmsg(_M_fd, &msg, flags); 
         }
 
         int 
         sendto(const void *buf, size_t len, int flags, const sockaddress<FAMILY> &to) 
-        { return _log("sendto",::sendto(_M_fd, buf, len, flags, 
-                                        reinterpret_cast<const struct sockaddr *>(&to), to.len())); }
+        { return ::sendto(_M_fd, buf, len, flags, 
+                          reinterpret_cast<const struct sockaddr *>(&to), to.len()); }
 
         int 
         recvfrom(void *buf, size_t len, int flags, sockaddress<FAMILY> &from) 
-        { return _log("recvfrom", ::recvfrom(_M_fd, buf, len, flags, 
-                                             reinterpret_cast<const struct sockaddr *>(&from), &from.len())); }
+        { return ::recvfrom(_M_fd, buf, len, flags, 
+                            reinterpret_cast<const struct sockaddr *>(&from), &from.len()); }
 
         virtual int 
         connect(const sockaddress<FAMILY> &addr)
-        { return _log("connect", ::connect(_M_fd, reinterpret_cast<const struct sockaddr *>(&addr), addr.len())); }
+        { return ::connect(_M_fd, reinterpret_cast<const struct sockaddr *>(&addr), addr.len()); }
 
         virtual int 
         bind(const sockaddress<FAMILY> &my_addr)
-        { return _log("bind", ::bind(_M_fd,reinterpret_cast<const struct sockaddr *>(&my_addr), my_addr.len()) ); }
+        { return ::bind(_M_fd,reinterpret_cast<const struct sockaddr *>(&my_addr), my_addr.len()); }
 
         int 
-        accept(sockaddress<FAMILY> &addr, base_socket<FAMILY, LOG> &ret) 
+        accept(sockaddress<FAMILY> &addr, base_socket<FAMILY, ATOMICITY> &ret) 
         {
-            int r = _log("accept", ::accept(_M_fd,reinterpret_cast<struct sockaddr *>(&addr), &addr.len()));
-            if (r != -1) 
+            int r = ::accept(_M_fd,reinterpret_cast<struct sockaddr *>(&addr), &addr.len());
+            if (r != -1) { 
+                typename ATOMICITY::scoped_lock sl(_M_mutex);
+                if (ret._M_fd != -1)
+                    ::close(ret._M_fd);
                 ret._M_fd = r;
+            }
             return r;
         }
 
         int 
         listen(int backlog) 
-        { return _log("listen", ::listen(_M_fd, backlog)); }
+        { return ::listen(_M_fd, backlog); }
 
         int 
         getsockname(sockaddress<FAMILY> &name) const
-        { return _log("getsockname", ::getsockname(_M_fd, reinterpret_cast<const struct sockaddr *>(&name), &name.len())); }
+        { return ::getsockname(_M_fd, reinterpret_cast<const struct sockaddr *>(&name), &name.len()); }
 
         int 
         getpeername(sockaddress<FAMILY> &name) const
-        { return _log("getpeername", ::getpeername(_M_fd, reinterpret_cast<const struct sockaddr *>(&name), &name.len())); }
+        { return ::getpeername(_M_fd, reinterpret_cast<const struct sockaddr *>(&name), &name.len()); }
 
         int 
         setsockopt(int level, int optname, const void *optval, socklen_t optlen)
-        { return _log("setsockopt", ::setsockopt(_M_fd, level, optname, optval, optlen)); }
+        { return ::setsockopt(_M_fd, level, optname, optval, optlen); }
 
         int 
         getsockopt(int level, int optname, void *optval, socklen_t *optlen) const
-        { return _log("getsockopt", ::getsockopt(_M_fd, level, optname, optval, optlen)); }
+        { return ::getsockopt(_M_fd, level, optname, optval, optlen); }
 
         const int 
         fd() const 
         { return _M_fd; }        
 
+        operator bool() const
+        { return _M_fd  == -1 ? false : true; }
+
+        int 
+        init(int type,int protocol=0) 
+        {
+            typename ATOMICITY::scoped_lock sl(_M_mutex);
+
+            if (_M_fd != -1) {
+                std::clog  << "base_socket::init: socket already open" << std::endl;
+                return -1;
+            }
+
+            _M_fd = ::socket(FAMILY, type, protocol);
+            if (_M_fd == -1) {
+                std::clog  << "base_socket::init: " << strerror(errno) << std::endl;
+                return -1;
+            }
+            return 0;
+        }
+
     protected:
-        int _M_fd;
+
+        mutable int _M_fd;
+        typename ATOMICITY::mutex  _M_mutex;
 
         base_socket() 
-        : _M_fd(-1)
+        : _M_fd(-1),
+          _M_mutex()
         {}
 
         base_socket(__socket_type type, int protocol=0)
-        : _M_fd(::socket(FAMILY, type, protocol))
+        : _M_fd(::socket(FAMILY, type, protocol)),
+          _M_mutex()
         {
             if ( _M_fd == -1) {
-                std::clog  << "socket: " << strerror(errno) << std::endl;
+                std::clog << "base_socket: " << strerror(errno) << std::endl;
             }
         }
 
-        base_socket(const base_socket &);                       // noncopyable
-        base_socket &operator=(const base_socket &);            // noncopyable
+        base_socket(const base_socket &rhs)
+        : _M_fd(),
+          _M_mutex()
+        {
+            typename ATOMICITY::scoped_lock sl(_M_mutex);
+            _M_fd = rhs._M_fd;
+            rhs._M_fd = -1;
+        }
+
+        base_socket &operator=(const base_socket &rhs)
+        {
+            typename ATOMICITY::scoped_lock sl(_M_mutex);
+            if (this != &rhs) {
+                _M_fd = rhs._M_fd;
+                rhs._M_fd = -1;
+            }
+            return *this;         
+        }
 
         virtual ~base_socket()
         {
+            typename ATOMICITY::scoped_lock sl(_M_mutex);
             if ( _M_fd != -1 ) {
-                _log("close",::close(_M_fd));
+                ::close(_M_fd);
             }
         }
 
-        int _log(const char *prefix, int ret)
-        {
-            if ( LOG && ret == -1) 
-                std::clog  << prefix << ": " << strerror(errno) << std::endl;
-            return ret;
-        }
     };
 
     // generic socket PF_INET/PF_INET6
     //
 
-    template <int FAMILY, bool LOG = true> 
-    struct socket : public base_socket<FAMILY, LOG> 
+    template <int FAMILY, typename ATOMICITY = default_socket_atomicity_policy> 
+    struct socket : public base_socket<FAMILY, ATOMICITY> 
     {
+        socket()
+        : base_socket<FAMILY, ATOMICITY>()
+        {}
+
         socket(__socket_type type, int protocol=0)
-        : base_socket<FAMILY,LOG>(type,protocol) 
+        : base_socket<FAMILY,ATOMICITY>(type,protocol) 
         {}
 
     };
 
     // PF_UNIX specializations...
     //
-    template <bool LOG>
-    struct socket<PF_UNIX, LOG> : public base_socket<PF_UNIX, LOG>   
+    template <typename ATOMICITY>
+    struct socket<PF_UNIX, ATOMICITY> : public base_socket<PF_UNIX, ATOMICITY>   
     {
+        socket()
+        : base_socket<PF_UNIX, ATOMICITY>()
+        {}
+
         socket(__socket_type type, int protocol=0)
-        : base_socket<PF_UNIX,LOG>(type,protocol),
+        : base_socket<PF_UNIX, ATOMICITY>(type,protocol),
         _M_pathname(),
         _M_bound(false)
         {}
@@ -178,13 +214,13 @@ namespace more {
         ~socket() 
         {
             if (!_M_pathname.empty() && _M_bound) {
-                base_socket<PF_UNIX,LOG>::_log("unlink", ::unlink(_M_pathname.c_str()));
+                ::unlink(_M_pathname.c_str());
             }
         }
 
         int bind(const sockaddress<PF_UNIX> &my_addr)
         {
-            int r = _log("bind", ::bind(this->_M_fd,reinterpret_cast<const struct sockaddr *>(&my_addr), my_addr.len()));
+            int r = ::bind(this->_M_fd,reinterpret_cast<const struct sockaddr *>(&my_addr), my_addr.len());
             if (r != -1)
                 _M_pathname = my_addr;
             return r;
@@ -192,7 +228,7 @@ namespace more {
 
         int connect(const sockaddress<PF_UNIX> &addr)
         {
-            int r = _log("connect", ::connect(this->_M_fd, reinterpret_cast<const struct sockaddr *>(&addr), addr.len()));
+            int r = ::connect(this->_M_fd, reinterpret_cast<const struct sockaddr *>(&addr), addr.len());
             if (r != -1)
                 _M_pathname = addr;
             return r;
