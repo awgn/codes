e95f733dc3e9d5dc0a86a075c56ae6ee96481158
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2009-02-14 18:22:15 +0000

    imported
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@51 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes++/colorful-test.cc
@@ -0,0 +1,23 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli 
+ * ----------------------------------------------------------------------------
+ */
+
+#include <iostream>
+#include <colorful.hh>
+
+using namespace more;
+
+int
+main(int argc, char *argv[])
+{
+    std::cout << colorful< TYPELIST(ecma::bold, ecma::fg_red) >()  << "hello" << 
+                 colorful< TYPELIST(ecma::bold, ecma::fg_blue) >() << " world" << colorful< TYPELIST(ecma::reset) >() << "!\n";
+    return 0;
+}
+ 
