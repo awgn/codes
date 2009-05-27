70d6b0133e79336fb263cf1d8313a01f054391b7
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2009-05-27 18:15:01 +0000

    added the support for non-default constructible classes, up to 5 generic parameters
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@104 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes++/factory-test.cc
+++ trunk/codes++/factory-test.cc
@@ -17,47 +17,102 @@ struct base
     virtual void hello_world()=0;
 };
 
+////////////////  default constructible classes  //////////////// 
+
 struct der1 : public base 
 {
     der1() : base() 
-    {}
+    { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+
+    ~der1()
+    { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+    
     virtual void hello_world() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
 };
 
 struct der2 : public base 
 {
     der2() : base() 
-    {}
+    { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+
+    ~der2()
+    { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+ 
     virtual void hello_world() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
 };
 
+////////////////  1 parameter-constructible classes (up to 5)  //////////////// 
 
-more::factory<std::string, base> factory;
+struct der3 : public base 
+{
+    der3(const std::string &n) : base() 
+    { std::cout << __PRETTY_FUNCTION__ << " -> " << n << std::endl; }
 
-namespace {
-    more::factory_register<der1, base> _void_(factory,"der1");
+    ~der3() 
+    { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+
+    virtual void hello_world() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+};
+
+struct der4 : public base 
+{
+    der4(const std::string &m) : base() 
+    { std::cout << __PRETTY_FUNCTION__ << " -> " << m << std::endl; }
+
+    ~der4() 
+    { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+
+    virtual void hello_world() { std::cout << __PRETTY_FUNCTION__ << std::endl; }
+};
+
+////////////////  global factories: simple and 1-parameter  //////////////// 
+
+more::factory<std::string, base> factory_0;                 // simple factory
+more::factory<std::string, base, std::string> factory_1;    // factory of objects whose constructors accept a std::string 
+
+namespace 
+{
+    // elegant registration: by means of the factory_register object
+
+    more::factory_register<base, der1> _void_1(factory_0,"der1");
+    more::factory_register<base, der2> _void_2(factory_0,"der2");
+    more::factory_register<base, der3> _void_3(factory_1,"der3", std::string());
+    more::factory_register<base, der4> _void_4(factory_1,"der4", std::string());
 }
 
 int
 main(int argc, char *argv[])
-{
-    // factory.regist("der1", new more::factory_allocator<der1, base> );
-    factory.regist("der2", new more::factory_allocator<der2, base> );
+{    
+    // basic registration: by invoking the regist() method on the factory object, providing the "id" and the factory_allocator
 
-    std::tr1::shared_ptr<base> p1 = factory("der1");
-    std::tr1::shared_ptr<base> p2 = factory("der2");
+    // factory_0.regist("der1", new more::factory_allocator<base, der1> );
+    // factory_0.regist("der2", new more::factory_allocator<base, der2> );
+    // factory_1.regist("der3", new more::factory_allocator<base, der3, std::string> );
+    // factory_1.regist("der4", new more::factory_allocator<base, der4, std::string> );
 
-    if (!p1) {
-        std::cout << "object not registered!" << std::endl;
+    std::cout << "--- start here ---" << std::endl;
+
+    {
+        std::tr1::shared_ptr<base> p1 = factory_0("der1");
+        std::tr1::shared_ptr<base> p2 = factory_0("der2");
+        std::tr1::shared_ptr<base> p3 = factory_1("der3",std::string("hello"));
+        std::tr1::shared_ptr<base> p4 = factory_1("der4",std::string("world"));
+
+        p1->hello_world();
+        p2->hello_world();
+        p3->hello_world();
+        p4->hello_world();
     }
+
+    std::cout << "--- out of scope ---" << std::endl;
+
+    factory_0.unregist("der2");
+    factory_1.unregist("der4");
     
-    p1->hello_world();
-    p2->hello_world();
-
-    factory.unregist("der2");
-
-    std::cout << std::boolalpha << "     -> der1 registration: " <<  factory.is_registered("der1") << std::endl;
-    std::cout << std::boolalpha << "     -> der2 registration: " <<  factory.is_registered("der2") << std::endl;
+    std::cout << std::boolalpha << "der1 is_registered: " <<  factory_0.is_registered("der1") << std::endl;
+    std::cout << std::boolalpha << "der2 is_registered: " <<  factory_0.is_registered("der2") << std::endl;
+    std::cout << std::boolalpha << "der3 is_registered: " <<  factory_1.is_registered("der3") << std::endl;
+    std::cout << std::boolalpha << "der4 is_registered: " <<  factory_1.is_registered("der4") << std::endl;
 
     return 0;
 }
--- trunk/codes++/factory.hh
+++ trunk/codes++/factory.hh
@@ -19,71 +19,194 @@ namespace more {
 
     using std::tr1::shared_ptr;
 
-    template <typename B>
+    namespace factory_util {
+
+        struct null_type
+        {};
+
+        template <int n>
+        struct int2type {
+            enum { value = n };
+        };
+
+        template <typename T>
+        struct length;
+
+        template < template <typename, typename, typename, typename, typename, typename> class E>
+        struct length < E<null_type, null_type, null_type, null_type, null_type, null_type> >
+        {
+            enum { value = 0 };
+        };
+        template < template <typename, typename, typename, typename, typename, typename> class E, 
+                   typename P0>
+        struct length < E<P0, null_type, null_type, null_type, null_type, null_type> >
+        {
+            enum { value = 1 };
+        };
+        template < template <typename, typename, typename, typename, typename, typename> class E, 
+                   typename P0, typename P1>
+        struct length < E<P0, P1, null_type, null_type, null_type, null_type> >
+        {
+            enum { value = 2 };
+        };
+        template < template <typename, typename, typename, typename, typename, typename> class E, 
+                   typename P0, typename P1, typename P2>
+        struct length < E<P0, P1, P2, null_type, null_type, null_type> >
+        {
+            enum { value = 3 };
+        };
+        template < template <typename, typename, typename, typename, typename, typename> class E, 
+                   typename P0, typename P1, typename P2, typename P3> 
+        struct length < E<P0, P1, P2, P3, null_type, null_type> >
+        {
+            enum { value = 4 };
+        };
+        template < template <typename, typename, typename, typename, typename, typename> class E, 
+                   typename P0, typename P1, typename P2, typename P3, typename P4> 
+        struct length < E<P0, P1, P2, P3, P4, null_type> >
+        {
+            enum { value = 5 };
+        };
+        template < template <typename, typename, typename, typename, typename, typename> class E, 
+                   typename P0, typename P1, typename P2, typename P3, typename P4, typename P5> 
+        struct length < E<P0, P1, P2, P3, P4, P5> >
+        {
+            enum { value = 6 };
+        };
+
+    }
+
+    template <typename B, typename P0 = factory_util::null_type, 
+                          typename P1 = factory_util::null_type, 
+                          typename P2 = factory_util::null_type, 
+                          typename P3 = factory_util::null_type, 
+                          typename P4 = factory_util::null_type >
     struct factory_base_allocator
     {
         virtual ~factory_base_allocator() {}
-        virtual B * alloc() = 0;
+        virtual B * alloc(P0 p0 = factory_util::null_type(),
+                          P1 p1 = factory_util::null_type(), 
+                          P2 p2 = factory_util::null_type(),
+                          P3 p3 = factory_util::null_type(),
+                          P4 p4 = factory_util::null_type()) = 0;
     };
 
-    template <typename T, typename B>   // B must be the base class of T and T must be default constructible 
-    struct factory_allocator : public factory_base_allocator<B>
+    template <typename B, typename D, typename P0 = factory_util::null_type, 
+                                      typename P1 = factory_util::null_type, 
+                                      typename P2 = factory_util::null_type, 
+                                      typename P3 = factory_util::null_type, 
+                                      typename P4 = factory_util::null_type
+                                      >   // B must be the base class of T 
+    struct factory_allocator : public factory_base_allocator<B,P0,P1,P2,P3,P4>
     {
-        virtual T * alloc() {
-            return new T;
+        virtual D * alloc(P0 p0, P1 p1,P2 p2,P3 p3,P4 p4)
+        {
+            return alloc__(factory_util::int2type<factory_util::length<factory_base_allocator<B,P0,P1,P2,P3,P4> >::value >(), p0, p1, p2, p3, p4);
         }
+
+        template <typename T0, typename T1, typename T2, typename T3, typename T4>    
+        D * alloc__(factory_util::int2type<1>, T0, T1, T2, T3, T4)
+        {   
+            return new D;
+        }
+        template <typename T0, typename T1, typename T2, typename T3, typename T4>    
+        D * alloc__(factory_util::int2type<2>, T0 p0, T1, T2, T3, T4)
+        {   
+            return new D(p0);
+        }
+        template <typename T0, typename T1, typename T2, typename T3, typename T4>    
+        D * alloc__(factory_util::int2type<3>, T0 p0, T1 p1, T2, T3, T4)
+        {   
+            return new D(p0,p1);
+        }
+        template <typename T0, typename T1, typename T2, typename T3, typename T4>    
+        D * alloc__(factory_util::int2type<4>, T0 p0, T1 p1, T2 p2, T3, T4)
+        {   
+            return new D(p0,p1,p2);
+        }
+        template <typename T0, typename T1, typename T2, typename T3, typename T4>    
+        D * alloc__(factory_util::int2type<5>, T0 p0, T1 p1, T2 p2, T3 p3, T4)
+        {   
+            return new D(p0,p1,p2,p3);
+        }
+        template <typename T0, typename T1, typename T2, typename T3, typename T4>    
+        D * alloc__(factory_util::int2type<6>, T0 p0, T1 p1, T2 p2, T3 p3, T4 p4)
+        {   
+            return new D(p0,p1,p2,p3,p4);
+        }
+ 
     };
 
     /////////////////////////////////////////////////////////////////////
     // utility: auto-register the allocator of E element (derived from B)
     //                to the F factory
 
-    template <typename E  /* element */,  
-              typename B  /* base element */>
+    template <typename B  /* element */,  
+              typename E  /* base element */
+              >
     struct factory_register
     {
         template <typename F /* factory */>
         factory_register(F &f, const std::string &k)
         {
-            f.regist(k, new typename more::factory_allocator<E, B>);    
+            f.regist(k, new typename more::factory_allocator<B, E>);    
+        }        
+        template <typename T0, typename F /* factory */>
+        factory_register(F &f, const std::string &k, T0)
+        {
+            f.regist(k, new typename more::factory_allocator<B, E, T0>);    
         }
+        template <typename T0, typename T1, typename F /* factory */>
+        factory_register(F &f, const std::string &k, T0, T1)
+        {
+            f.regist(k, new typename more::factory_allocator<B, E, T0, T1>);    
+        }
+        template <typename T0, typename T1, typename T2, typename F /* factory */>
+        factory_register(F &f, const std::string &k, T0, T1, T2)
+        {
+            f.regist(k, new typename more::factory_allocator<B, E, T0, T1, T2>);    
+        }
+        template <typename T0, typename T1, typename T2, typename T3, typename F /* factory */>
+        factory_register(F &f, const std::string &k, T0, T1 ,T2 ,T3)
+        {
+            f.regist(k, new typename more::factory_allocator<B, E, T0, T1, T2, T3>);    
+        }
+        template <typename T0, typename T1, typename T2, typename T3, typename T4, typename F /* factory */>
+        factory_register(F &f, const std::string &k, T0, T1, T2, T3, T4)
+        {
+            f.regist(k, new typename more::factory_allocator<B, E, T0, T1, T2, T3, T4>);    
+        }
+ 
     };
 
     /////////////////////////////////////////
     // factory class: K:key -> T:base_element
 
-    template <typename K, typename T>
+    template <typename K, typename T, typename P0 = factory_util::null_type, 
+                                      typename P1 = factory_util::null_type, 
+                                      typename P2 = factory_util::null_type, 
+                                      typename P3 = factory_util::null_type, 
+                                      typename P4 = factory_util::null_type >
     class factory
     {
 
     public:
-        typedef std::map<K, shared_ptr<factory_base_allocator<T> > > factoryMap;
+        typedef std::map<K, shared_ptr< factory_base_allocator<T,P0,P1,P2,P3,P4> > > factoryMap;
 
         factory()
         : _M_map()
         {}
 
         ~factory()
-        {
-            // typename factoryMap::const_iterator it = _M_map.begin();
-            // for(;it != _M_map.end(); ++it) 
-            //     delete it->second;
-        }
+        {}
 
         bool
-        regist(const K & key, factory_base_allocator<T> * value)
-        { return _M_map.insert( make_pair(key, shared_ptr<factory_base_allocator<T> >(value) ) ).second; }
-
+        regist(const K & key, factory_base_allocator<T,P0,P1,P2,P3,P4> * value)
+        { return _M_map.insert( make_pair(key, shared_ptr<factory_base_allocator<T,P0,P1,P2,P3,P4> >(value) ) ).second; }
+        
         bool
         unregist(const K &key)
-        {
-            // typename factoryMap::const_iterator it = _M_map.find(key);
-            // if (it == _M_map.end())
-            //     return false;
-            // delete it->second;
-
-            return _M_map.erase(key) == 1;
-        }
+        { return _M_map.erase(key) == 1; }
 
         bool
         is_registered(const K &key) const
@@ -101,6 +224,56 @@ namespace more {
             if (it == _M_map.end())
                 return shared_ptr<T>();
             return shared_ptr<T>(it->second->alloc());
+        }
+
+        template <typename T0>
+        shared_ptr<T> 
+        operator()(const K &key, T0 t0) const
+        {
+            typename factoryMap::const_iterator it = _M_map.find(key);
+            if (it == _M_map.end())
+                return shared_ptr<T>();
+            return shared_ptr<T>(it->second->alloc(t0));
+        }
+
+        template <typename T0, typename T1>
+        shared_ptr<T> 
+        operator()(const K &key, T0 t0, T1 t1) const
+        {
+            typename factoryMap::const_iterator it = _M_map.find(key);
+            if (it == _M_map.end())
+                return shared_ptr<T>();
+            return shared_ptr<T>(it->second->alloc(t0,t1));
+        }
+
+        template <typename T0, typename T1, typename T2>
+        shared_ptr<T> 
+        operator()(const K &key, T0 t0, T1 t1, T2 t2) const
+        {
+            typename factoryMap::const_iterator it = _M_map.find(key);
+            if (it == _M_map.end())
+                return shared_ptr<T>();
+            return shared_ptr<T>(it->second->alloc(t0,t1,t2));
+        }
+
+        template <typename T0, typename T1, typename T2, typename T3>
+        shared_ptr<T> 
+        operator()(const K &key, T0 t0, T1 t1, T2 t2, T3 t3) const
+        {
+            typename factoryMap::const_iterator it = _M_map.find(key);
+            if (it == _M_map.end())
+                return shared_ptr<T>();
+            return shared_ptr<T>(it->second->alloc(t0,t1,t2,t3));
+        }
+
+        template <typename T0, typename T1, typename T2, typename T3, typename T4>
+        shared_ptr<T> 
+        operator()(const K &key, T0 t0, T1 t1, T2 t2, T3 t3, T4 t4) const
+        {
+            typename factoryMap::const_iterator it = _M_map.find(key);
+            if (it == _M_map.end())
+                return shared_ptr<T>();
+            return shared_ptr<T>(it->second->alloc(t0,t1,t2,t3,t4));
         }
 
     private:
