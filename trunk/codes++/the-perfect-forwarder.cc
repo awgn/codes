3f262f055fd8b89439ad6bd04fabe156d1acef4e
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2009-05-31 18:17:20 +0000

    done
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@112 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes++/the-perfect-forwarder.cc
@@ -0,0 +1,305 @@
+/* $Id*/
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli 
+ * ----------------------------------------------------------------------------
+ */
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+/*****************************************************************************************************************
+
+ [*] --- All the things you have always wanted to know about writing a generic perfect forwarder --- [*]
+
+
+ definition: A generic forwarder (wrapper) is a template function whose final objective is to forward the 
+             passed arguments to a given target function or callable object, provided as extra argument. 
+
+             A forwarder is perfect if its call is equivalent to passing its arguments directly to the target function: 
+             the behavior is correct and sports the same performance, not involving any further copy constructor.
+
+ hypothesis: The current C++ standard does not allow to implement a generic perfect forwarder. A perfect forwarder is 
+             indeed enabled by means of C++0x rvalue references. Instead, a generic non-perfect but correct forwarded 
+             is implementable with the help of tr1::reference_wrapper<>. 
+
+ thesis:     The major challenge of writing a generic wrapper is to provide a mechanism for it to pass its 
+             arguments to a target function that accepts them by non-const reference.
+
+             The client of the wrapper (the callee) must be able to pass both lvalue and rvalue as arguments, 
+             in accordance with the target signature. Obviously if a target parameter (a parameter of the target function) 
+             is taken as non-const reference, the corresponding actual parameter must not be provided to the wrapper 
+             as unnamed object or rvalue.
+
+             In the study presented, the following free function has been chosen as target object (it could be an object
+             class providing the overloaded operator()() const, or any other callable object):
+
+             int fun(arg a1, const arg & a2, arg & a3)
+             {
+                a3 = arg(1);
+                ...
+                return 0;
+             }
+
+             The first signature examined as candidate for the perfect wrapper is the following one:
+  
+             template <typename F, typename T1, typename T2, typename T3>
+             int candidate_forwarder_1(F &f, T1 &t1, T2 &t2, T3 &t3)
+             {
+                     return f(t1,t2,t3);
+             }
+
+             with all the arguments passed by non-const reference.
+              
+             The candidate_forwarder_1 sports the theoretical performance in that no additional copy constructors 
+             are called during the parameter passing (comparisons are provided by test #0 and #1).
+         
+             However such a wrapper has a limitation: it does not allow the callee to pass an rvalue as argument 
+             even if this argument is intended to be passed to the target function by value or const reference. 
+             
+             This limitation is imposed by the standard that does not allow the non-const reference of the wrapper 
+             to bind an rvalue, to prevent the common error that modifyng an rvalue does not make much sense.
+
+             To remove this limitation, a second candidate for the perfect forwarder is taken into account:
+
+             template <typename F, typename T1, typename T2, typename T3>
+             int candidate_forwarder_2(F f, T1 t1, T2 t2, T3 t3)
+             {
+                 return f(t1,t2,t3);
+             }
+
+             [ note: this is the decaying version of candidate_forwarder_1, as defined in 
+                     C++ Template: the complete guide (Vandevoorde/Josuttis) ]
+
+             with all the arguments passed by value. At first glance it seems that the ban of rvalue passed as argument 
+             is removed and indeed it is. But just because it's the decaying version, arguments are copy-constructed and 
+             more importantly the type deduction follows the rule that array and function types are deducted as pointer types 
+             and that top-level CV qualifiers (const/volatile) are discarded.
+             
+             Consequently this second wrapper has the following problems: 
+
+             *1) it's not a perfect forwarder due to the decaying deduction. The deducted arguments, indeed, cannot be used 
+                 to select a possible overloaded target function (feature not required in this study). 
+
+             *2) parameters of the target function taken by reference are bound to the formal parameters of the forwarder 
+                 (t3, in the example) and not to actual ones provided by the callee (a3 in the test), which is very bad!
+
+             *3) parameters are passed by value and additional copy constructors are called.
+
+             The test #2 shows the major problem (*2) that makes the wrapper incorrect: the argument a3 passed to the wrapper 
+             is not updated by the target function as it would be if it were passed directly to the target function.
+            
+             Fortunately a workaround to this problem does exists. The std::tr1::reference_wrapper<> is the helper class 
+             the callee must use for those arguments intended to be passed as non-const reference to the target function.
+          
+             The common instantiation point will be the following one:
+
+             candidate_forwarder_2(fun, a1, a2, std::tr1::ref(a3));
+
+             Test #3 and #4 show the correctness of the forwarder function achieved by means of the tr1::reference_wrapper<>. 
+            
+             The comparison of the test #0 and #4 shows further copy constructors involved for passing a1 and a2 (*3). 
+             This is price to pay to have a correct wrapper that can accept rvalues where the target function does.
+
+             Also note that test #3 does not show such additional copy constructors thanks to certain optimizations
+             of temporary/unnamed objects that takes place during the argument passing:
+
+                               copy constructor are optimized away
+                                          /       /
+             candidate_forwarder_2(fun, arg(), arg(), std::tr1::ref(a3) );
+
+             Despite of the performance loss the test #4 shows, candidate_forwarder_2 function is preferable to 
+             candidate_forwarder_1, in that the limitation of rvalue passed as argument is removed. Even if if it's not 
+             perfect at all (*1), it's the most convenient implementation the current C++ standard allows with the help of TR1.
+
+             The the perfect forwarder is enabled by means of C++0x rvalue reference and has the following form:
+
+             template <typename F, typename T1, typename T2, typename T3>
+             int the_perfect_forwarder(F f, T1 && t1, T2 && t2, T3 && t3)
+             {
+                 return f(std::forward<T1>(t1),
+                          std::forward<T2>(t2),
+                          std::forward<T3>(t3));
+             }
+
+             The limitation that a non-const reference cannot bind a rvalue is overcome by means of the rvalue reference [&&]
+             which can binds to rvalue and allows its modification.
+
+             This wrapper has the following properties:
+
+             *) Argument deduction is done in the following way: if an lvalue is passed, than Tn is deducted as an lvalue reference; 
+                if an rvalue is passed, then Tn is deducted as plain type and the std::forward<>() helper is required for 
+                the wrapper to be perfect.
+             *) Both lvalue and rvalue can be provided as argument to the wrapper, in accordance to the signature of the
+                target function/object. 
+             *) Additional copy constructors are not involeved.
+
+             Nicola -- 09/05/31
+
+*****************************************************************************************************************/
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include <tr1/functional>
+#include <iostream>
+#include <colorful.hh>
+
+using namespace more;
+
+typedef colorful< TYPELIST(ecma::bold, ecma::fg_red) > red;
+typedef colorful< TYPELIST(ecma::bold, ecma::fg_blue) > blue;
+typedef colorful< TYPELIST(ecma::reset) > rst;
+typedef colorful< TYPELIST(ecma::bold) > bold;
+
+struct arg 
+{
+    arg(int n = 0)
+    : _M_value(n)
+    { std::cout << "    " << __PRETTY_FUNCTION__ << std::endl; }
+
+    ~arg()
+    { std::cout << "    " << __PRETTY_FUNCTION__ << std::endl; }
+    
+    arg(const arg &rhs)
+    : _M_value(rhs._M_value)
+    { 
+        std::cout << "    " << __PRETTY_FUNCTION__ << std::endl; 
+    }
+
+    arg &operator=(const arg &rhs)
+    { 
+        _M_value = rhs._M_value;
+        std::cout << "    " << __PRETTY_FUNCTION__ << std::endl; 
+    }
+    
+    int get() const
+    { return _M_value; }
+
+    int _M_value;
+};
+
+
+/////////////////////////////////////////////////////////////
+// the target function:
+
+int fun(arg a1, const arg & a2, arg & a3)
+{
+    a3 = arg(1);       
+    std::cout << "fun: a1 = " << a1.get() << std::endl;
+    std::cout << "fun: a2 = " << a2.get() << std::endl;
+    std::cout << "fun: a3 = " << a3.get() << std::endl;
+    return 0;
+}
+
+/////////////////////////////////////////////////////////////
+// candidates:
+
+template <typename F, typename T1, typename T2, typename T3>
+int candidate_forwarder_1(F &f, T1 &t1, T2 &t2, T3 &t3)
+{
+    return f(t1,t2,t3);
+}   
+
+template <typename F, typename T1, typename T2, typename T3>
+int candidate_forwarder_2(F f, T1 t1, T2 t2, T3 t3)
+{
+    return f(t1,t2,t3);
+} 
+
+#ifdef __GXX_EXPERIMENTAL_CXX0X__
+template <typename F, typename T1, typename T2, typename T3>
+int the_perfect_forwarder(F f, T1 && t1, T2 && t2, T3 && t3)
+{
+    return f(std::forward<T1>(t1),
+             std::forward<T2>(t2),
+             std::forward<T2>(t3));
+} 
+#endif
+
+/////////////////////////////////////////////////////////////
+// test case:
+
+int
+main(int argc, char *argv[])
+{    
+    
+    std::cout << red() << "\n[#0] direct call" << rst() << std::endl;
+    {
+        arg a1;
+        arg a2;
+        arg a3;
+
+        std::cout << "--- prolog:" << std::endl;
+        fun(a1, a2, a3);  // rvalue are not allowed
+        std::cout << "--- epilog:" << std::endl;
+
+        std::cout << "    arg & a3 = " << a3.get() << " <== should be 1! [" << bold() << "OK" << rst() << "]" << std::endl;
+    }
+
+
+    std::cout << red() << "\n[#1] candidate_forwarder_1" << rst() << std::endl;
+    {
+        arg a1;
+        arg a2;
+        arg a3;
+
+        std::cout << "--- prolog:" << std::endl;
+        candidate_forwarder_1(fun, a1, a2, a3);  // rvalue are not allowed
+        std::cout << "--- epilog:" << std::endl;
+
+        std::cout << "    arg & a3 = " << a3.get() << " <== should be 1! [" << bold() << "OK" << rst() << "]" << std::endl;
+    }
+
+    std::cout << red() << "\n[#2] candidate_forwarder_2" << rst() << std::endl;
+    {
+        arg a3;
+
+        std::cout << "--- prolog:" << std::endl;
+        candidate_forwarder_2(fun, arg(), arg(), a3 );  // rvalue are not allowed
+        std::cout << "--- epilog:" << std::endl;
+
+        std::cout << "    arg & a3 = " << a3.get() << " <== should be 1! [" << red() << "ERROR" << rst() << "]" << std::endl;
+    }
+
+    std::cout << red() << "\n[#3] candidate_forwarder_2 [with std::tr1::reference_wrapper<>]" << rst() << std::endl;
+    {
+        arg a3;
+
+        std::cout << "--- prolog:" << std::endl;
+        candidate_forwarder_2(fun, arg(), arg(), std::tr1::ref(a3) );  // rvalue are not allowed
+        std::cout << "--- epilog:" << std::endl;
+
+        std::cout << "    arg & a3 = " << a3.get() << " <== should be 1! [" << bold() << "OK" << rst() << "]" << std::endl;
+    }
+
+    std::cout << red() << "\n[#4] candidate_forwarder_2 [with std::tr1::reference_wrapper<>]" << rst() << std::endl;
+    {
+        arg a1;
+        arg a2;
+        arg a3;
+
+        std::cout << "--- prolog:" << std::endl;
+        candidate_forwarder_2(fun, a1, a2, std::tr1::ref(a3) );  // rvalue are not allowed
+        std::cout << "--- epilog:" << std::endl;
+
+        std::cout << "    arg & a3 = " << a3.get() << " <== should be 1! [" << bold() << "OK" << rst() << "]" << std::endl;
+    }
+
+#ifdef __GXX_EXPERIMENTAL_CXX0X__    
+    std::cout << blue() << "\n[#5] the_perfect_forwarder [c++0x right reference]" << rst() << std::endl;
+    {
+        arg a1;
+        arg a2;
+        arg a3;
+
+        std::cout << "--- prolog:" << std::endl;
+        the_perfect_forwarder(fun, a1, a2, a3 );  // rvalue are not allowed
+        std::cout << "--- epilog:" << std::endl;
+
+        std::cout << "    arg & a3 = " << a3.get() << " <== should be 1! [" << bold() << "OK" << rst() << "]" << std::endl;
+    }
+
+#endif
+    return 0;
+}
+
