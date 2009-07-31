439377b4151303d85f504d9345db80d5d4887f24
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2009-07-31 20:12:51 +0000

    major update
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@178 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes++/timer-test.cc
@@ -0,0 +1,189 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
+#include <iostream>
+
+#include <timer.hh>
+#include <signal.h>
+
+using namespace more::time;
+
+more::time::tick_type global_tick; // external linkage
+
+template <tick_type & Tc> 
+struct Thread_1 : public more::posix::thread, private ticker<Tc, itimer_trait<ITIMER_REAL>::signal_expiration > 
+{    
+    Thread_1()
+    {}
+
+    ~Thread_1()
+    {
+        this->cancel();
+    }
+
+    void *operator()()
+    {
+        uint32_t ts;
+        for(int i=0; i < 20; ++i) {
+            this->wait_for_tick(ts);  // timer
+
+            std::cout << std::hex << 
+            "ITIMER_REAL: [" << this->get_id() << "] " << std::dec << i << " << tick = {" << std::dec << Tc.current << "}\n";
+        }
+
+        return NULL;
+    }
+};
+
+
+template <tick_type & Tc> 
+struct Thread_2 : public more::posix::thread, private ticker<Tc, SIGRT_MIN > 
+{    
+    Thread_2()
+    {}
+
+    ~Thread_2()
+    {
+        this->cancel();
+    }
+
+    void *operator()()
+    {
+        uint32_t ts;
+        for(int i=0; i < 20; ++i) {
+            this->wait_for_tick(ts);  // timer
+
+            std::cout << std::hex << 
+            "SIGRT_MIN: [" << this->get_id() << "] " << std::dec << i << " << tick = {" << std::dec << Tc.current << "}\n";
+        }
+
+        return NULL;
+    }
+};
+
+template <tick_type & Tc> 
+struct Thread_3 : public more::posix::thread, private ticker<Tc, SIGRT_MIN+1 > 
+{    
+    Thread_3()
+    {}
+
+    ~Thread_3()
+    {
+        this->cancel();
+    }
+
+    void *operator()()
+    {
+        uint32_t ts;
+        for(int i=0; i < 20; ++i) {
+            this->wait_for_tick(ts);  // timer
+
+            std::cout << std::hex << 
+            "SIGRT_MIN+1: [" << this->get_id() << "] " << std::dec << i << " << tick = {" << std::dec << Tc.current << "}\n";
+        }
+
+        return NULL;
+    }
+};
+
+
+
+using namespace more::posix;
+
+int
+main(int argc, char *argv[])
+{    
+    ////////////////////////////////////
+    // 1) block sigalarm in this thread 
+
+    sigset_t block;
+    sigemptyset(&block);
+    sigaddset(&block, SIGALRM);
+    sigaddset(&block, SIGVTALRM);
+    sigaddset(&block, SIGPROF);
+
+    sigaddset(&block, SIGRT_MIN);
+    sigaddset(&block, SIGRT_MIN+1);
+
+    this_thread::sig_mask(SIG_BLOCK, &block, NULL);
+
+    ///////////////////////////////
+    // 2) create the itimer thread
+
+    timeval t = { 0, 10000 };
+    more::time::itimer_thread<ITIMER_REAL,    global_tick > x(&t);
+
+    ///////////////////
+    // and start it ... 
+    x.start();
+
+    //////////////////////
+    // 3) create threads
+
+    more::posix::thread * p = new Thread_1<global_tick>;
+    p->start(); 
+    p->join();
+
+
+    //
+    // REALTIME timers
+    //
+ 
+    itimerspec spec, spec2;
+
+    spec.it_value.tv_sec  = 0;
+    spec.it_value.tv_nsec = 100000000;
+    spec.it_interval.tv_sec  = spec.it_value.tv_sec;
+    spec.it_interval.tv_nsec = spec.it_value.tv_nsec;
+
+    spec2.it_value.tv_sec  = 0;
+    spec2.it_value.tv_nsec = 200000000;
+    spec2.it_interval.tv_sec  = spec2.it_value.tv_sec;
+    spec2.it_interval.tv_nsec = spec2.it_value.tv_nsec;
+
+  
+    {
+        std::cout << "simple rt_timer test: " << std::endl;
+
+        more::time::rt_timer<CLOCK_REALTIME, SIGEV_SIGNAL, SIGRT_MIN   > x;
+        more::time::rt_timer<CLOCK_REALTIME, SIGEV_SIGNAL, SIGRT_MIN+1 > y;
+
+        x.set(&spec);
+        y.set(&spec2);
+
+        sleep(1);
+
+        std::cout << "done." << std::endl;
+    }
+
+    {
+        std::cout << "rt_timer_thread test: " << std::endl;
+
+        more::time::rt_timer_thread<SIGRT_MIN,   global_tick > rt_1(&spec); 
+        more::time::rt_timer_thread<SIGRT_MIN+1, global_tick > rt_2(&spec2); 
+        
+        rt_1.start();
+        rt_2.start();
+
+        more::posix::thread * p = new Thread_2<global_tick>;
+        more::posix::thread * q = new Thread_3<global_tick>;
+
+        p->start(); 
+        q->start(); 
+
+        q->join();
+        p->join();
+
+        std::cout << "done." << std::endl;
+    }
+
+    return 0;
+}
+ 
---
+++ trunk/codes++/timer.hh
@@ -0,0 +1,411 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef _TIMER_HH_
+#define _TIMER_HH_ 
+
+#include <pthread++.hh>
+#include <tr1/type_traits>
+#include <tr1/array>
+#include <cstring>
+
+#include <sys/time.h>
+#include <errno.h>
+
+namespace more { namespace time {
+
+    namespace 
+    {
+        template <bool N> struct static_assert;
+        template <>
+        struct static_assert<true>
+        {
+            enum { value = true };
+        };
+    }
+
+    struct tick_data
+    {
+        typedef struct {        
+            posix::mutex _mutex;
+            posix::cond  _cond;
+        } tick_data_type;
+
+        std::tr1::array<tick_data_type, 64 /* under linux SIGRTMAX is a function and cannot appear in constant expressions */ +1 > thread_data; 
+    };
+
+    ////////////////////////////////////////////
+    // ... to be declared with external linkage
+
+    struct tick_type : public tick_data
+    {
+        volatile uint32_t current;
+    };
+
+    //////////////////////////////////////////////////////
+    // threads derive privately from ticker class in order
+    // to inherit wait_for_tick() member function.
+
+    template <tick_type & T, int sig>
+    struct ticker
+    {
+        void wait_for_tick(uint32_t &last)
+        {
+            posix::scoped_lock<posix::mutex> lock(T.thread_data[sig]._mutex);
+            if (last == T.current)
+                T.thread_data[sig]._cond.wait(lock);
+            last = T.current;
+        }
+    };
+
+    ////////////////////////////////////////////////////////////////////////
+    // itimer and itimer_thread 
+
+    template <int w> struct itimer_trait;
+    template <> struct itimer_trait<ITIMER_REAL>
+    {
+        enum { signal_expiration = SIGALRM };
+    };
+
+    template <> struct itimer_trait<ITIMER_VIRTUAL> 
+    {
+        enum { signal_expiration = SIGVTALRM };
+    };
+
+    template <> struct itimer_trait<ITIMER_PROF> 
+    {
+        enum { signal_expiration = SIGPROF };
+    };
+
+    //////////////////
+    // itimer object
+
+    template <int WHICH>
+    class itimer
+    {
+    public:
+
+        itimer()
+        : _M_itv(), _M_itv_p(NULL)
+        {}
+
+        itimer(const itimerval * value, itimerval *ovalue = NULL)
+        : _M_itv(*value), _M_itv_p(ovalue)
+        {}
+
+        itimer(const timeval *value)
+        : _M_itv(), _M_itv_p(0)
+        {
+            this->set(value);
+        }
+
+        itimer(time_t sec, suseconds_t usec)
+        : _M_itv(), _M_itv_p(0)
+        {
+            this->set(sec,usec);
+        }
+
+        ~itimer()
+        {
+            // stop the itimer
+            itimerval stop = {{0, 0}, {0, 0}};
+            ::setitimer(WHICH, &stop, 0);
+        }
+        
+        void operator()()
+        {
+            // start the itimer
+            if ( ::setitimer(WHICH, &_M_itv, _M_itv_p) < 0 )
+                throw std::runtime_error(std::string("setitimer: ").append(strerror(errno)));
+        }
+
+        void set(const itimerval * value, itimerval *ovalue = NULL)
+        { 
+            _M_itv = * value; 
+            _M_itv_p = ovalue;
+        }
+
+        void set(const struct timeval *value)
+        {
+            _M_itv.it_interval.tv_sec  = value->tv_sec;
+            _M_itv.it_interval.tv_usec = value->tv_usec;
+            _M_itv.it_value.tv_sec     = value->tv_sec;
+            _M_itv.it_value.tv_usec    = value->tv_usec;
+        }
+
+        void set(time_t sec, suseconds_t usec)
+        {
+            _M_itv.it_interval.tv_sec  = sec;
+            _M_itv.it_interval.tv_usec = usec;
+            _M_itv.it_value.tv_sec     = sec;
+            _M_itv.it_value.tv_usec    =usec;
+        }
+
+        int get(struct itimerval *value) const
+        { 
+            return ::getitimer(WHICH,value); 
+        }
+
+    private: 
+        itimerval   _M_itv;
+        itimerval  *_M_itv_p;
+    };
+
+
+    template <int WHICH, tick_type & T>
+    class itimer_thread : public posix::thread 
+    {
+
+    public:
+        itimer_thread()
+        : _M_itimer()
+        {}
+
+        itimer_thread(const struct itimerval *value, struct itimerval *oldvalue = NULL)
+        : _M_itimer(value, oldvalue)
+        {}
+
+        itimer_thread(const struct timeval *value)
+        : _M_itimer(value)
+        {}
+
+        itimer_thread(time_t sec, suseconds_t usec)
+        : _M_itimer(sec, usec)
+        {}
+
+        ~itimer_thread()
+        {
+            this->cancel();
+        }
+
+        void *operator()()
+        {
+            int sig;
+
+            // start the timer
+            _M_itimer();
+
+            sigset_t sigexp; 
+            sigemptyset(&sigexp);
+            sigaddset(&sigexp, itimer_trait<WHICH>::signal_expiration);
+
+            for(timeval now;;) 
+            {
+                sigwait(&sigexp, &sig);
+                assert( sig == itimer_trait<WHICH>::signal_expiration );
+                ::gettimeofday(&now, NULL);
+
+                posix::scoped_lock<posix::mutex> lock(T.thread_data[ itimer_trait<WHICH>::signal_expiration  ]._mutex);
+                T.current = now.tv_sec * 1000 + now.tv_usec / 1000;
+                T.thread_data[ itimer_trait<WHICH>::signal_expiration ]._cond.broadcast();
+            }
+            return (void *)0;        
+        }
+
+    private:
+
+        itimer<WHICH>  _M_itimer;
+
+        // non-copyable idiom
+        itimer_thread(const itimer_thread &);
+        itimer_thread & operator=(const itimer_thread &);
+    };
+
+    //////////////////////////////////////////////////////////////////////////////////////////
+    //////////////////////////////////////// REALTIME ////////////////////////////////////////
+    //////////////////////////////////////////////////////////////////////////////////////////
+
+    static const int SIGRT_MIN = 34;
+    static const int SIGRT_MAX = 64;
+    
+    template <clockid_t CID, int SIGEV = SIGEV_SIGNAL, int SIGNO = SIGRT_MAX, int FLAGS = 0> 
+    //
+    // CID   = CLOCK_REALTIME, CLOCK_MONOTONIC, CLOCK_PROCESS_CPUTIME_ID, CLOCK_THREAD_CPUTIME_ID
+    // SIGEV = SIGEV_SIGNAL, SIGEV_NONE, SIGEV_THREAD, SIGEV_THREAD_ID 
+    // FLAGS = 0, TIMER_ABSTIME  
+    //
+    class rt_timer        
+    { 
+    public:
+
+        rt_timer()
+        : _M_id(), _M_its(), _M_its_p(0)
+        {
+            this->init();
+        }
+
+        rt_timer(const itimerspec *value, itimerspec *oldvalue = NULL)
+        : _M_id(), _M_its(*value), _M_its_p(oldvalue)
+        {
+            this->init();
+        }
+
+        rt_timer(const timespec *value)
+        : _M_id(), _M_its(), _M_its_p(0)
+        {
+            this->set(value);
+            this->init();
+        }
+
+        rt_timer(time_t sec, long nsec)
+        : _M_id(), _M_its(), _M_its_p(0)
+        {
+            this->set(sec,nsec);
+            this->init();
+        }
+
+        ~rt_timer()
+        {
+            // stop the timer
+            ::timer_delete(_M_id); 
+        }
+
+        void operator()()
+        {
+            // start the timer
+            if (::timer_settime(_M_id, FLAGS, &_M_its, _M_its_p) < 0)
+                throw std::runtime_error(std::string("timer_settime: ").append(strerror(errno)));
+        }
+
+        timer_t
+        get_id() const
+        { return _M_id; }
+
+        void set(const itimerspec *value, itimerspec *oldvalue = NULL)
+        {
+            _M_its   = *value;
+            _M_its_p = oldvalue; 
+        } 
+
+        void set(const timespec *value)
+        {            
+            _M_its.it_interval.tv_sec  = value->tv_sec;
+            _M_its.it_interval.tv_nsec = value->tv_nsec; 
+            _M_its.it_value.tv_sec     = value->tv_sec;
+            _M_its.it_value.tv_nsec    = value->tv_nsec;
+ 
+            _M_its_p = NULL; 
+        } 
+        void set(time_t sec, long nsec)
+        {            
+            _M_its.it_interval.tv_sec  = sec;
+            _M_its.it_interval.tv_nsec = nsec; 
+            _M_its.it_value.tv_sec     = sec;
+            _M_its.it_value.tv_nsec    = nsec;
+
+            _M_its_p = NULL; 
+        } 
+
+        int get(itimerspec &value) const
+        {
+            return ::timer_gettime(_M_id, &value);
+        }
+
+    private:
+        void init()
+        {
+            static_assert< CID == CLOCK_REALTIME            ||
+                           CID == CLOCK_MONOTONIC           ||
+                           CID == CLOCK_PROCESS_CPUTIME_ID  ||
+                           CID == CLOCK_THREAD_CPUTIME_ID > clokid_not_allowed __attribute__((unused));
+
+            static_assert< SIGEV == SIGEV_SIGNAL ||
+                           SIGEV == SIGEV_NONE   ||
+                           SIGEV == SIGEV_THREAD ||
+                           SIGEV == SIGEV_THREAD_ID > sigev_notify_not_allowed __attribute__((unused));
+
+            // The  Linux  kernel  supports a range of 32 different real-time signals,
+            // numbered 33 to 64. However, the  glibc  POSIX  threads  implementation
+            // internally  uses  two  (for NPTL) or three (for LinuxThreads) real-time
+            // signals (see pthreads(7)), and adjusts the value of  SIGRTMIN  suitably
+            // (to 34 or 35)
+
+            static_assert< SIGNO >= SIGRT_MIN && SIGNO <= SIGRT_MAX > rt_sig_numer_not_allowed __attribute__((unused)); 
+
+            static_assert< FLAGS == 0 || FLAGS == 1 > timer_settime_flags_not_allowed __attribute__((unused));
+
+            struct sigevent ev;
+            ev.sigev_notify = SIGEV;
+            ev.sigev_signo  = SIGNO;
+            ev.sigev_value.sival_ptr = &_M_id;
+
+            if ( ::timer_create(CID, &ev, &_M_id) <  0 )
+                throw std::runtime_error(std::string("timer: ").append(strerror(errno)));
+        }
+
+        timer_t      _M_id;
+
+        itimerspec   _M_its;
+        itimerspec * _M_its_p;
+
+        // non-copyable idiom
+        rt_timer(const rt_timer &);
+        rt_timer & operator=(const rt_timer &);
+
+    };
+
+    template <int SIGNO, tick_type & T, clockid_t CID = CLOCK_REALTIME >
+    class rt_timer_thread : public posix::thread
+    {        
+    public:
+
+        rt_timer_thread()
+        : _M_rt_timer()
+        {}
+
+        rt_timer_thread(const itimerspec *value, itimerspec *oldvalue = NULL)
+        : _M_rt_timer(value, oldvalue)
+        {}
+
+        rt_timer_thread(const timespec *value)
+        : _M_rt_timer(value)
+        {}
+
+        rt_timer_thread(time_t sec, long nsec)
+        : _M_rt_timer(sec,nsec)
+        {}
+
+        virtual ~rt_timer_thread()
+        {
+            this->cancel();
+        }
+
+        void *operator()()
+        {
+            int sig;
+
+            // start the timer
+            _M_rt_timer();
+
+            sigset_t sigexp;
+            sigemptyset(&sigexp);
+            sigaddset(&sigexp, SIGNO);
+
+            for(timeval now;;)
+            {
+                sigwait(&sigexp, &sig);
+                assert( sig == SIGNO );
+                ::gettimeofday(&now, NULL);
+
+                posix::scoped_lock<posix::mutex> lock(T.thread_data[SIGNO]._mutex);
+                T.current = now.tv_sec * 1000 + now.tv_usec / 1000;
+                T.thread_data[SIGNO]._cond.broadcast();
+            }
+            return (void *)0;
+        }
+
+    private:
+        rt_timer<CID, SIGEV_SIGNAL, SIGNO>  _M_rt_timer;
+    };
+
+} // namespace time
+} // namespace more
+
+#endif /* _TIMER_HH_ */
