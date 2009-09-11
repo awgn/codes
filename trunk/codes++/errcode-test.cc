6fdca1710b1a483157f51ce7697587d977306a72
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2009-09-11 10:06:26 +0000

    imported
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@210 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes++/errcode-test.cc
@@ -0,0 +1,23 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
+#include <errcode.h>
+
+#include <iostream>
+#include <string>
+#include <cassert>
+
+int
+main(int argc, char *argv[])
+{
+    assert( std::string("ENOTRECOVERABLE") == more::errcode_str[ENOTRECOVERABLE] );
+    return 0;
+}
+ 
