ef05c92f348ec4643799496d9f154b32fc8a4627
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2009-09-29 18:54:05 +0000

    update
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@258 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes++0x/helloworld.cc
+++ trunk/codes++0x/helloworld.cc
@@ -1,16 +1,9 @@
 #include <iostream>
-#include <string>
-
-std::string helloworld()
-{
-    std::string ret("Hello World!");
-    return std::move(ret); 
-}
 
 int
 main(int argc, char *argv[])
 {
-    std::cout << helloworld() << std::endl;
+    static_assert(false, "Hello world!");
     return 0;
 }
  
