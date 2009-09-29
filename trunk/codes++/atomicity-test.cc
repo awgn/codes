21fbf124e4e2bd30fa9961329b82dae54631c05f
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2009-09-29 09:09:13 +0000

    added
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@250 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes++/atomicity-test.cc
@@ -0,0 +1,37 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ *
+ */
+
+#include <atomicity-policy.hh>
+#include <iostream>
+
+///////////////////////////////////////////////
+// compile with and without -pthread option...
+
+using namespace more;
+
+struct test : private atomicity::emptybase_mutex<atomicity::DEFAULT>
+{
+    int n;
+
+    void method()
+    {
+        atomicity::DEFAULT::scoped_lock L(this->mutex());
+    }
+};
+
+
+int
+main(int argc, char *argv[])
+{
+    std::cout << sizeof(test) << std::endl;
+    return 0;
+}
+ 
