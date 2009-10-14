8b39290e95663f4c8b2fa594fd5ec497c290fdcb
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2009-10-14 12:09:27 +0000

    major update
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@277 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes++/lazy_object-test.cc
+++ trunk/codes++/lazy_object-test.cc
@@ -14,6 +14,8 @@
 
 #include <lazy_object.hh>
 
+//////////////////// base <- derived ... 
+
 struct base 
 {
     base()
@@ -34,7 +36,7 @@ struct derived : public base
     derived(const std::string &h)
     : _M_msg(h)
     {
-        std::cout << __PRETTY_FUNCTION__ << ": " <<  h << std::endl;
+        std::cout << "`->" <<  __PRETTY_FUNCTION__ << std::endl;
     }
 
     ~derived()
@@ -51,9 +53,9 @@ struct derived : public base
 };
 
 
-typedef more::lazy_object<derived,std::string> lazy_derived;
-
 #include <tr1/functional>
+
+typedef more::lazy_object<derived> lazy_derived;
 
 int
 main(int argc, char *argv[])
@@ -63,8 +65,8 @@ main(int argc, char *argv[])
 
     std::cout << "[*] building the lazy_object<>..." << std::endl;
 
-    more::lazy_object<derived,std::string> x("hello");
-    more::lazy_object<derived,std::string> y("world");
+    more::lazy_object<derived> x(std::string("hello"));
+    more::lazy_object<derived> y(std::string("world"));
   
     vec.push_back(x);
     vec.push_back(y);
@@ -72,7 +74,15 @@ main(int argc, char *argv[])
     sleep(2);
      
     std::cout << "[*] building real objects..." << std::endl;
-    std::for_each(vec.begin(), vec.end(), std::tr1::mem_fn(&more::lazy_object<derived,std::string>::operator()) );
+
+    // to build the object call ctor<> function as:
+    //
+    // x.ctor() <- if the class has a default constructor
+    //
+    // x.ctor<std::string>(); <- invoke derived(const std::string &) constructor
+    //
+
+    std::for_each(vec.begin(), vec.end(), std::tr1::mem_fn(&more::lazy_object<derived>::ctor<std::string> /* ctor signature */ ) );
 
     sleep(2);
 
--- trunk/codes++/lazy_object.hh
+++ trunk/codes++/lazy_object.hh
@@ -11,70 +11,50 @@
 #ifndef _LAZY_OBJECT_HH_
 #define _LAZY_OBJECT_HH_ 
 
-#include <template_util.hh>
+#include <stdexcept>
+
 #include <shared_ptr.hh>
+#include <any.hh>
 
 namespace more {
 
-    template <typename T, typename T1 = null_type, typename T2 = null_type, typename T3 = null_type, 
-                          typename T4 = null_type, typename T5 = null_type, typename T6 = null_type >
-    struct object_allocator
-    {
-        static
-        T * alloc(const T1 & t1, const T2 & t2, const T3 & t3, 
-                  const T4 & t4, const T5 & t5, const T6 & t6)
-        {
-           return alloc(more::tag< count_param_6<T1,T2,T3,T4,T5,T6>::value >(), t1, t2, t3, t4, t5, t6); 
-        } 
-
-        static
-        T * alloc( more::tag<0>, const T1 &, const T2 &, const T3 &,const T4 &, const T5 &, const T6 &)
-        {
-            return new T;
-        }
-        static
-        T * alloc( more::tag<1>, const T1 &t1, const T2 &, const T3 &, const T4 &, const T5 &, const T6 &)
-        {
-            return new T(t1);
-        }
-        static
-        T * alloc( more::tag<2>, const T1 &t1, const T2 &t2, const T3 &, const T4 &, const T5 &, const T6 &)
-        {
-            return new T(t1,t2);
-        }
-        static
-        T * alloc( more::tag<3>, const T1 &t1, const T2 &t2, const T3 &t3, const T4 &, const T5 &, const T6 &)
-        {
-            return new T(t1,t2,t3);
-        }
-        static
-        T * alloc( more::tag<4>, const T1 &t1, const T2 &t2, const T3 &t3, const T4 &t4, const T5 &, const T6 &)
-        {
-            return new T(t1,t2,t3,t4);
-        }
-        static
-        T * alloc( more::tag<5>, const T1 &t1, const T2 &t2, const T3 &t3, const T4 &t4, const T5 &t5, const T6 &)
-        {
-            return new T(t1,t2,t3,t4,t5);
-        }
-        static
-        T * alloc( more::tag<6>, const T1 &t1, const T2 &t2, const T3 &t3, const T4 &t4, const T5 &t5, const T6 &t6)
-        {
-            return new T(t1,t2,t3,t4,t5,t6);
-        }
-
-    };
-
-    template <typename T, typename T1 = null_type, typename T2 = null_type, typename T3 = null_type, 
-                          typename T4 = null_type, typename T5 = null_type, typename T6 = null_type >
+    template <typename T> 
     class lazy_object
     {
     public:
 
-        lazy_object(const T1 & t1 = T1(), const T2 & t2 = T2(), const T3 & t3 = T3(), 
-                    const T4 & t4 = T4(), const T5 & t5 = T5(), const T6 & t6 = T6())
+        lazy_object()
+        : _M_obj()
+        {}
 
-        : _M_obj(), _M_t1(t1), _M_t2(t2), _M_t3(t3), _M_t4(t4), _M_t5(t5), _M_t6(t6)
+        template <typename T1>
+        lazy_object(const T1 &t1)
+        : _M_obj(), _M_arg1(t1)
+        {}
+
+        template <typename T1, typename T2>
+        lazy_object(const T1 &t1, const T2 &t2)
+        : _M_obj(), _M_arg1(t1), _M_arg2(t2)
+        {}
+
+        template <typename T1, typename T2, typename T3>
+        lazy_object(const T1 &t1, const T2 &t2, const T3 &t3)
+        : _M_obj(), _M_arg1(t1), _M_arg2(t2), _M_arg3(t3)
+        {}
+
+        template <typename T1, typename T2, typename T3, typename T4>
+        lazy_object(const T1 &t1, const T2 &t2, const T3 &t3, const T4 &t4)
+        : _M_obj(), _M_arg1(t1), _M_arg2(t2), _M_arg3(t3), _M_arg4(t4)
+        {}
+
+        template <typename T1, typename T2, typename T3, typename T4, typename T5>
+        lazy_object(const T1 &t1, const T2 &t2, const T3 &t3, const T4 &t4, const T5 &t5)
+        : _M_obj(), _M_arg1(t1), _M_arg2(t2), _M_arg3(t3), _M_arg4(t4), _M_arg5(t5)
+        {}
+
+        template <typename T1, typename T2, typename T3, typename T4, typename T5, typename T6>
+        lazy_object(const T1 &t1, const T2 &t2, const T3 &t3, const T4 &t4, const T5 &t5, const T6 &t6)
+        : _M_obj(), _M_arg1(t1), _M_arg2(t2), _M_arg3(t3), _M_arg4(t4), _M_arg5(t5), _M_arg6(t6)
         {}
 
         ~lazy_object()
@@ -82,15 +62,63 @@ namespace more {
 
         ///////////////// delayed constructor
 
-        void operator()()
+        void ctor()
         {
-            _M_obj = typename more::shared_ptr<T>::type
-                             (object_allocator<T,T1,T2,T3,T4,T5,T6>::alloc(_M_t1, _M_t2, _M_t3, _M_t4, _M_t5, _M_t6));
+            _M_obj = typename more::shared_ptr<T>::type( new T );
+        }
+
+        template <typename P1>
+        void ctor()
+        {
+            _M_obj = typename more::shared_ptr<T>::type( new T(more::any_cast<P1>(_M_arg1)) );
+        }
+        template <typename P1, typename P2>
+        void ctor()
+        {
+            _M_obj = typename more::shared_ptr<T>::type( new T(more::any_cast<P1>(_M_arg1),
+                                                               more::any_cast<P2>(_M_arg2)) );
+        }
+        template <typename P1, typename P2, typename P3>
+        void ctor()
+        {
+            _M_obj = typename more::shared_ptr<T>::type( new T(more::any_cast<P1>(_M_arg1),
+                                                               more::any_cast<P2>(_M_arg2),
+                                                               more::any_cast<P3>(_M_arg3)) );
+        }
+        template <typename P1, typename P2, typename P3, typename P4>
+        void ctor()
+        {
+            _M_obj = typename more::shared_ptr<T>::type( new T(more::any_cast<P1>(_M_arg1),
+                                                               more::any_cast<P2>(_M_arg2),
+                                                               more::any_cast<P3>(_M_arg3),
+                                                               more::any_cast<P4>(_M_arg4)) );
+        }
+        template <typename P1, typename P2, typename P3, typename P4, typename P5>
+        void ctor()
+        {
+            _M_obj = typename more::shared_ptr<T>::type( new T(more::any_cast<P1>(_M_arg1),
+                                                               more::any_cast<P2>(_M_arg2),
+                                                               more::any_cast<P3>(_M_arg3),
+                                                               more::any_cast<P4>(_M_arg4),
+                                                               more::any_cast<P5>(_M_arg5)) );
+        }
+        template <typename P1, typename P2, typename P3, typename P4, typename P5, typename P6>
+        void ctor()
+        {
+            _M_obj = typename more::shared_ptr<T>::type( new T(more::any_cast<P1>(_M_arg1),
+                                                               more::any_cast<P2>(_M_arg2),
+                                                               more::any_cast<P3>(_M_arg3),
+                                                               more::any_cast<P4>(_M_arg4),
+                                                               more::any_cast<P5>(_M_arg5),
+                                                               more::any_cast<P6>(_M_arg6)) );
         }
 
         typename more::shared_ptr<T>::type
         shared_from_this()
         {
+            if (!_M_obj)
+                throw std::logic_error("object not yet constructed");
+
             return typename more::shared_ptr<T>::type(_M_obj);
         }
 
@@ -98,8 +126,12 @@ namespace more {
 
         typename more::shared_ptr<T>::type _M_obj; // read why more::shared_prt<> in shared_ptr.hh 
 
-        T1  _M_t1; T2  _M_t2; T3  _M_t3;
-        T4  _M_t4; T5  _M_t5; T6  _M_t6;
+        more::any  _M_arg1;  
+        more::any  _M_arg2; 
+        more::any  _M_arg3;
+        more::any  _M_arg4; 
+        more::any  _M_arg5; 
+        more::any  _M_arg6;
     };
 }
 
