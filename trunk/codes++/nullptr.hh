bbb021214c9c6ead5cc4685ec9be8b9dd796b2a5
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2010-01-26 14:44:21 +0000

    nullptr added
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@409 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes++/nullptr.hh
@@ -0,0 +1,24 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef _NULLPTR_HH_
+#define _NULLPTR_HH_ 
+
+struct __nullptr
+{
+    template <typename T>
+    operator T *()
+    {
+        return 0;
+    }
+
+} extern nullptr;
+
+#endif /* _NULLPTR_HH_ */
