afe8c2d317a2898eb299518b3d7e63bbd699f29c
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2010-01-31 19:29:57 +0000

    algorithm: added
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@413 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes++/algorithm.hh
@@ -0,0 +1,95 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli 
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef _ALGORITHM_HH_
+#define _ALGORITHM_HH_ 
+
+#include <tr1/functional>
+#include <algorithm>
+
+namespace more { 
+
+    // logical_xor
+    //
+
+    template <typename T1, typename T2>
+    inline bool
+    logical_xor(const T1 &a, const T2 &b)
+    {
+        return static_cast<bool>(a) ^ static_cast<bool>(b);
+    } 
+
+    // chop
+    //
+
+    template <typename T>
+    struct chop : public std::unary_function<T,T>
+    {
+        chop(const T &_min, const T &_max)
+        : _M_min(_min), _M_max(_max)
+        {}
+
+        T operator()(const T &value) const
+        {
+            return value < _M_min ? (_M_min) : ( value > _M_max ? _M_max : value );
+        }
+
+        T _M_min;
+        T _M_max;
+    };
+
+    // flipflop predicate 
+    //
+ 
+    template <typename Arg>
+    struct flipflop : public std::unary_function<Arg, bool>
+    {
+        template <typename Pred>
+        flipflop(const Pred &p)
+        : _M_state(false), _M_pred(p)
+        {}
+
+        bool
+        operator()(const Arg &value) const
+        {
+            return _M_state = logical_xor( _M_state, _M_pred(value) );
+        }
+
+        mutable bool _M_state;
+        std::tr1::function< bool(Arg) > _M_pred;
+    };
+
+    // flipflop2 predicate
+    //
+
+    template <typename Arg>
+    struct flipflop2 : public std::unary_function<Arg, bool>
+    {
+        template <typename Pred1, typename Pred2>
+        flipflop2(const Pred1 &p1, const Pred2 &p2)
+        : _M_state(false), _M_pred1(p1), _M_pred2(p2)
+        {}
+
+        bool
+        operator()(const Arg &value) const
+        {
+            return _M_state = _M_state ? logical_xor(_M_state, _M_pred2(value)) :
+                                         logical_xor(_M_state, _M_pred1(value)) ; 
+        }
+
+        mutable bool _M_state;
+
+        std::tr1::function< bool(Arg) > _M_pred1;
+        std::tr1::function< bool(Arg) > _M_pred2;
+    };
+
+
+} // namespace more
+#endif /* _ALGORITHM_HH_ */
