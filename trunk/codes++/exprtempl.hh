25b6ca5098e96ffed77471c74621a67f4dd47ddf
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2010-02-05 17:49:56 +0000

    expression template: generators are enabled only on expression template types
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@432 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes++/exprtempl.hh
+++ trunk/codes++/exprtempl.hh
@@ -11,6 +11,8 @@
 #ifndef _EXPRTEMPL_HH_
 #define _EXPRTEMPL_HH_ 
 
+#include <mtp.hh>
+
 namespace more { namespace expr { 
 
     // unary expression template...
@@ -20,21 +22,21 @@ namespace more { namespace expr {
     class unary_expr
     {
     public:
-        typedef typename Arg::value_type value_type;
+        typedef typename Arg::expression_type expression_type;
 
         unary_expr(Arg arg, Op op = Op())
         : _M_arg(arg),
         _M_op(op)
         {}
 
-        value_type 
+        expression_type 
         eval() const 
         {
             return _M_op(_M_arg.eval());
         }
 
         template <typename C>
-        value_type 
+        expression_type 
         eval(const C &ctx) const 
         {
             return _M_op(_M_arg.eval(ctx));
@@ -70,20 +72,20 @@ namespace more { namespace expr {
     class binary_expr
     {
     public:
-        typedef typename equal<typename T1::value_type,typename T2::value_type>::type value_type;
+        typedef typename equal<typename T1::expression_type,typename T2::expression_type>::type expression_type;
 
         binary_expr(T1 _l, T2 _r, Op _op = Op() )
         : _M_lhs(_l), _M_rhs(_r), _M_op(_op)
         {}
 
-        value_type
+        expression_type
         eval() const
         {
             return _M_op(_M_lhs.eval(), _M_rhs.eval());
         }
 
         template <typename C>
-        value_type
+        expression_type
         eval(const C &ctx) const
         {
             return _M_op(_M_lhs.eval(ctx), _M_rhs.eval(ctx));
@@ -114,7 +116,7 @@ namespace more { namespace expr {
     template <typename T, T value>
     struct integral
     {
-        typedef T value_type;
+        typedef T expression_type;
 
         T eval() const
         { return value; }
@@ -406,93 +408,128 @@ namespace more { namespace expr {
         { return "/"; }
     };
 
-
+    //////////////////
     // generators...
     //
 
+    template <typename T>
+    class is_expression_template
+    {
+        typedef char __one;
+        typedef struct { char __arr[2]; } __two;
+        
+        template <typename C> static __one test(typename C::expression_type *);
+        template <typename C> static __two test(...);
+
+    public:
+        enum { value = sizeof ( test<T>(0) ) == sizeof(__one) };
+    };
+
+    template <typename T, typename Ty>
+    class enable_if_expression 
+        : public more::mtp::enable_if_c< is_expression_template<T>::value, Ty> {};
+
+    template <typename T1, typename T2, typename Ty>
+    class enable_if_expression_2 
+        : public more::mtp::enable_if_c< is_expression_template<T1>::value && is_expression_template<T2>::value, Ty > {};
+
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_or>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_or> >::type
     operator|(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_or>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_logical_or>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_logical_or> >::type
     operator||(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_logical_or>(_l, _r);
     }
 
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_and>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_and> >::type
     operator&(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_and>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_logical_and>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_logical_and> >::type
     operator&&(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_logical_and>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_xor>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_xor> >::type
     operator^(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_xor>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_logical_xor>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_logical_xor> >::type
     logical_xor(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_logical_xor>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_eq>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_eq> >::type
     operator==(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_eq>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_not_eq>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_not_eq> >::type
     operator!=(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_not_eq>(_l, _r);
     }
 
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_greater>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_greater> >::type
     operator>(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_greater>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_greater_eq>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_greater_eq> >::type
     operator>=(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_greater_eq>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_less>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_less> >::type
     operator<(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_less>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_less_eq>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_less_eq> >::type
     operator<=(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_less_eq>(_l, _r);
     }
  
     template <typename T>
-    unary_expr<T, op_not>
+    typename enable_if_expression<T,
+             unary_expr<T, op_not> >::type
     operator!(T el)
     {
         return unary_expr<T, op_not>(el);
     }
     template <typename T>
-    unary_expr<T, op_compl>
+    typename enable_if_expression<T,
+             unary_expr<T, op_compl> >::type
     operator~(T el)
     {
         return unary_expr<T, op_compl>(el);
@@ -501,25 +538,29 @@ namespace more { namespace expr {
     // +-*/...
 
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_add>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_add> >::type
     operator+(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_add>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_sub>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_sub> >::type 
     operator-(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_sub>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_mul>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_mul> >::type
     operator*(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_mul>(_l, _r);
     }
     template <typename T1, typename T2>
-    binary_expr<T1, T2, op_div>
+    typename enable_if_expression_2<T1,T2, 
+             binary_expr<T1, T2, op_div> >::type
     operator/(T1 _l, T2 _r)
     {
         return binary_expr<T1, T2, op_div>(_l, _r);
@@ -546,7 +587,7 @@ namespace more { namespace expr {
     //
 
     template <typename T>
-    typename T::value_type 
+    typename T::expression_type 
     eval(T exp)
     {
         return exp.eval();
@@ -556,7 +597,7 @@ namespace more { namespace expr {
     //
 
     template <typename T, typename C>
-    typename T::value_type 
+    typename T::expression_type 
     eval(T exp, const C & ctx)
     {
         return exp.eval(ctx);
