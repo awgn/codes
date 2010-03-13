0d764f011d7b8f47b2b668de09d55b7b222e8b1f
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2010-03-13 12:32:52 +0000

    value_ptr: imported
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@467 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes.cpp/value_ptr-test.cc
@@ -0,0 +1,68 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
+#include <iostream>
+#include <value_ptr.hh>
+
+struct base 
+{
+    virtual 
+    base * clone() const = 0; 
+};
+
+struct derived : public base
+{
+    derived *
+    clone() const
+    {
+        return new derived(*this);
+    }
+};
+
+
+int
+main(int argc, char *argv[])
+{
+    more::value_ptr<int> a(new int(1));
+
+    more::value_ptr<int, more::transfer_ptr> b(new int(2));
+    std::cout << "b:" << static_cast<void *>(b.get()) << std::endl;
+
+    more::value_ptr<int, more::transfer_ptr> c(b);
+    
+    std::cout << "b:" << static_cast<void *>(b.get()) << std::endl;
+    std::cout << "c:" << static_cast<void *>(c.get()) << std::endl;
+
+    b = c;    
+
+    std::cout << "b:" << static_cast<void *>(b.get()) << std::endl;
+    std::cout << "c:" << static_cast<void *>(c.get()) << std::endl;
+
+    more::value_ptr<int,more::deepcopyable_ptr> abc ( new int(10) );
+    more::value_ptr<int,more::deepcopyable_ptr> xxx = abc;
+
+    std::cout << *xxx << std::endl;
+    std::cout << *abc << std::endl;
+
+    * xxx = 20;
+    * abc = 30;
+
+    std::cout << *xxx << std::endl;
+    std::cout << *abc << std::endl;
+
+    // cloneable: object must provide
+    // the virtual object * clone() const method
+
+    more::value_ptr<base, more::cloneable_ptr> x( new derived );
+    more::value_ptr<base, more::cloneable_ptr> q = x;
+
+    return 0;
+}
+ 
