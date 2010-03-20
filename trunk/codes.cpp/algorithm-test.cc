8e8bbf528e9b9d25701a659b37b4f4d84cf3034c
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2010-03-20 12:19:49 +0000

    functional added, algorithm updated
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@478 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes.cpp/algorithm-test.cc
+++ trunk/codes.cpp/algorithm-test.cc
@@ -17,41 +17,6 @@ using namespace std::placeholders;
 int
 main(int argc, char *argv[])
 {
-    // logical xor
-    std::cout << "logical_xor: " << more::logical_xor(0,0) << std::endl;
-    std::cout << "logical_xor: " << more::logical_xor(1,0) << std::endl;
-    std::cout << "logical_xor: " << more::logical_xor(0,1) << std::endl;
-    std::cout << "logical_xor: " << more::logical_xor(1,42) << std::endl;
-
-    // chop
-    more::chop<int> chopper(0,2);
-    std::cout << "chop: " << chopper(-1) << std::endl;
-    std::cout << "chop: " << chopper(1) << std::endl;
-    std::cout << "chop: " << chopper(3) << std::endl;
-
-    // flipflop
-    more::flipflop<int> ff1( (std::logical_not<int>()) );
-
-    std::cout << "flipflop: " << ff1(1) << std::endl; 
-    std::cout << "flipflop: " << ff1(1) << std::endl; 
-    std::cout << "flipflop: " << ff1(0) << std::endl; 
-    std::cout << "flipflop: " << ff1(1) << std::endl; 
-    std::cout << "flipflop: " << ff1(1) << std::endl; 
-    std::cout << "flipflop: " << ff1(0) << std::endl; 
-    std::cout << "flipflop: " << ff1(1) << std::endl; 
-    std::cout << "flipflop: " << ff1(1) << std::endl; 
-
-    // flipflop2
-    more::flipflop2<int> ff2( std::bind(std::equal_to<int>(),_1, 0), std::bind(std::equal_to<int>(), _1, 3) );    
-
-    std::cout << "flipflop2: " << -2 << " " << ff2(-2) << std::endl;
-    std::cout << "flipflop2: " << -1 << " " << ff2(-1) << std::endl;
-    std::cout << "flipflop2: " << 0  << " " << ff2(0)  << std::endl;
-    std::cout << "flipflop2: " << 1  << " " << ff2(1)  << std::endl;
-    std::cout << "flipflop2: " << 2  << " " << ff2(2)  << std::endl;
-    std::cout << "flipflop2: " << 3  << " " << ff2(3)  << std::endl;
-    std::cout << "flipflop2: " << 4  << " " << ff2(4)  << std::endl;
-
     // levenshtein distance
     //
 
