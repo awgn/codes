2f5a6f7092b48ebd3c6badd034f542914d6e54cf
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2010-03-24 19:08:18 +0000

    imported shared_queue, fast_deque deprecated
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@491 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes.cpp/shared_queue-test.cc
@@ -0,0 +1,90 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
+#include <pthread.h>
+
+#include <shared_queue.hh>
+#include <cassert>
+
+class elem 
+{
+    int _M_elem;
+public:
+
+    elem (int _e) 
+    : _M_elem(_e)
+    {}
+
+    ~elem()
+    {}
+
+    int value()
+    { 
+        return _M_elem; 
+    }
+};
+
+more::shared_queue<elem *,8> queue;    
+
+void *thread_producer(void *)
+{
+    for(int i=0; i<100000; ) {
+
+        elem * p = new elem(i);
+retry:
+        if ( queue.push_back(p) ) {
+            std::cout << "push: value->(" << i << ")" << std::endl; 
+            i++;
+            usleep(1000);
+            continue;
+        }
+        usleep(1000);
+        goto retry;
+    }
+
+    return NULL;
+}
+
+void *thread_consumer(void *)
+{
+    elem *r;
+    for(;;) {
+        if (!queue.pop_front(r) )
+        {
+            sched_yield();
+            continue;
+        }
+
+        assert(r);
+        std::cout << "pop: " << r->value() <<  " (size = " << queue.size() << ")" <<std::endl;
+        delete r;
+        usleep(5000);
+    }
+
+    return NULL;
+}
+
+int main()
+{
+    pthread_t a;
+    pthread_t b;
+    pthread_t c;
+    pthread_t d;
+
+    pthread_create(&a, NULL, thread_producer, NULL);
+    pthread_create(&b, NULL, thread_consumer, NULL);
+    pthread_create(&c, NULL, thread_consumer, NULL);
+    pthread_create(&c, NULL, thread_consumer, NULL);
+
+    pthread_join(a,NULL);
+    pthread_join(b,NULL);
+    pthread_join(c,NULL);
+    pthread_join(d,NULL);
+}
---
+++ trunk/codes.cpp/shared_queue.hh
@@ -0,0 +1,125 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef SHARED_QUEUE_HH
+#define SHARED_QUEUE_HH
+
+#include <noncopyable.hh>       // more!
+#include <static_assert.hh>     // more!
+#include <spinlock.hh>          // more!
+
+#ifndef __GXX_EXPERIMENTAL_CXX0X__
+#include <tr1/type_traits>
+namespace std { using namespace std::tr1; }
+#else
+#include <tr1/type_traits>
+#endif
+
+#include <vector>
+
+namespace more 
+{
+    template <bool N> struct shared_queue_element;
+    template <>
+    struct shared_queue_element<true>
+    {};
+
+    template <typename T, unsigned int N = 8, unsigned int Producer = 1, unsigned int Consumer = 1> 
+    class shared_queue : more::noncopyable, shared_queue_element< std::tr1::has_nothrow_assign<T>::value && 
+                                                                  std::tr1::has_nothrow_copy<T>::value  >
+    {
+        public:
+            typedef typename std::vector<T>::size_type  size_type;
+            typedef typename std::vector<T>::value_type value_type;
+            typedef unsigned int unsigned_type;
+
+        private:
+
+            unsigned_type _M_head;
+            unsigned_type _M_tail;
+
+            std::vector<value_type> _M_storage;
+
+            more::spinlock<more::lock_smart<> > _M_head_lock;
+            more::spinlock<more::lock_smart<> > _M_tail_lock;
+
+            unsigned_type 
+            mod_N(unsigned_type n)
+            {
+                return n & (N-1);
+            }
+        
+        public:
+            shared_queue() 
+            : _M_head(0), _M_tail(0), _M_storage(N) 
+            {
+                static_assert((N & (N-1)) == 0, not_a_power_of_two); 
+            }
+
+            ~shared_queue() 
+            {}
+            
+            bool 
+            pop_front(T &ret) 
+            {
+                more::scoped_spinlock< more::lock_smart<> > _lock_(_M_tail_lock);
+                if ( _M_tail == _M_head )
+                   return false;
+
+                ret = _M_storage[_M_tail];
+                _M_tail = mod_N(_M_tail+1);
+                return true;
+            }
+
+            bool 
+            push_back(const T & elem)  
+            {
+                more::scoped_spinlock< more::lock_smart<> > _lock_(_M_head_lock);
+                unsigned_type next = mod_N(_M_head+1);
+
+                if ( next == _M_tail )
+                   return false;
+                
+                _M_storage[_M_head] = elem;
+                _M_head = next;
+                return true;
+            }
+
+            void 
+            clear() 
+            { 
+                more::scoped_spinlock< more::lock_smart<> > _lock_(_M_tail_lock);
+                int _M_tail = _M_head;
+            }
+            
+            bool
+            empty() const 
+            { 
+                return _M_head == _M_tail; 
+            }
+
+            size_type 
+            max_size() const 
+            { 
+                return N; 
+            }
+
+            size_type 
+            size() const 
+            {
+                return _M_head >= _M_tail ? (_M_head - _M_tail) : (N +_M_head - _M_tail);
+            }
+
+        };
+
+} // namespace more
+
+#endif /* SHARED_QUEUE_HH */
+
