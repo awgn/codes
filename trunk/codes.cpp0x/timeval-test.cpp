e4064600a26755169028c2d1fcf7eca1d74bec53
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2010-08-28 21:23:06 +0000

    added timeval class
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@547 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes.cpp0x/timeval-test.cpp
@@ -0,0 +1,93 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli 
+ * ----------------------------------------------------------------------------
+ */
+
+#include <timeval.hpp>
+
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <vector>
+
+using namespace more;
+
+int
+main(int argc, char *argv[])
+{
+    more::Timeval a( Timeval::now() );
+
+    Timeval b;    
+
+    std::cout << "a = " << a << std::endl;    
+    std::cout << "b = " << b << std::endl;        
+
+    if(!b) {
+        std::cout << "b is not set! (ok)" << std::endl; 
+    }
+ 
+    b = a;
+
+    std::cout << "b = " << b << std::endl;        
+
+    Timeval half(0,500000);
+
+    b+= half;
+
+    std::cout << "b = " << b << std::endl;        
+
+    std::cout << "b - 0.5 sec = " << ( b - half ) << std::endl;
+
+    b.update();
+
+    std::cout << "b = " << b << std::endl;        
+
+    usleep(500000);
+
+    Timeval c( Timeval::now() );
+
+    std::cout << "c = " << c << std::endl;        
+
+    std::cout << "diff = " << (c-b) << std::endl;
+
+    std::cout << std::boolalpha << (c == c) << std::endl;
+
+    std::cout << (c > b)  << std::endl;
+    std::cout << (c >= b) << std::endl;
+    std::cout << (c >= c) << std::endl;
+
+    std::cout << (c != b) << std::endl;    
+    
+    std::cout << (b < c ) << std::endl;
+    std::cout << (b <= c) << std::endl;
+    std::cout << (b <= b) << std::endl;
+
+    std::vector<more::Timeval> vec;
+
+    vec.push_back(c);
+    vec.push_back(b);
+
+    std::sort(vec.begin(), vec.end());
+
+    std::cout << "vec: ";
+    std::copy(vec.begin(), vec.end(), std::ostream_iterator<Timeval>(std::cout, " "));
+    std::cout << std::endl;
+
+    std::cout << "b.to_usec() = " << b.to_usec() << std::endl;
+
+    std::cout << "b = " << b << std::endl;        
+    std::cout << "c = " << c << std::endl; 
+    
+    std::swap(b,c);
+
+    std::cout << "b = " << b << std::endl;        
+    std::cout << "c = " << c << std::endl; 
+
+    return 0;
+}
+ 
---
+++ trunk/codes.cpp0x/timeval.hpp
@@ -0,0 +1,231 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli 
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef _TIMEVAL_HPP_
+#define _TIMEVAL_HPP_
+
+#include <sys/time.h>
+
+#include <iostream>
+#include <algorithm>
+
+namespace more { 
+
+    class Timeval
+    {
+    public:
+
+        typedef void(Timeval::*bool_type)();
+
+        Timeval()
+        : _M_tv()
+        {}
+
+        Timeval(time_t sec, suseconds_t usec)
+        {
+            _M_tv.tv_sec = sec;
+            _M_tv.tv_usec = usec;    
+        }
+
+        ~Timeval()
+        {}
+
+        Timeval(const Timeval& rhs)
+        {
+            _M_tv.tv_sec = rhs.tv_sec();
+            _M_tv.tv_usec = rhs.tv_usec();
+        }
+
+        explicit Timeval(const struct timeval& rhs)
+        {
+            _M_tv.tv_sec = rhs.tv_sec;
+            _M_tv.tv_usec = rhs.tv_usec;
+        }
+
+        explicit Timeval(const struct timespec& rhs)
+        {
+            _M_tv.tv_sec = rhs.tv_sec;
+            _M_tv.tv_usec = rhs.tv_nsec/1000;
+        }
+        
+        void 
+        swap(Timeval &rhs) 
+        {   
+            std::swap(_M_tv.tv_sec, rhs._M_tv.tv_sec);
+            std::swap(_M_tv.tv_usec, rhs._M_tv.tv_usec);
+        }
+
+        Timeval & operator=(const Timeval &rhs)
+        { 
+            _M_tv.tv_sec = rhs.tv_sec();
+            _M_tv.tv_usec = rhs.tv_usec();
+            return *this;
+        }
+
+        Timeval & operator=(const timeval &rhs)
+        { 
+            _M_tv.tv_sec = rhs.tv_sec;
+            _M_tv.tv_usec = rhs.tv_usec;
+            return *this;
+        }
+
+        Timeval & operator=(const timespec &rhs)
+        { 
+            _M_tv.tv_sec = rhs.tv_sec;
+            _M_tv.tv_usec = rhs.tv_nsec/1000;
+            return *this;
+        }
+
+        Timeval & operator+=(const Timeval &rhs)
+        {   
+            _M_tv.tv_sec +=  rhs.tv_sec();
+            _M_tv.tv_usec += rhs.tv_usec();
+
+            if (_M_tv.tv_usec >= 1000000) {
+                ++_M_tv.tv_sec;
+                _M_tv.tv_usec -= 1000000;
+            }
+            return *this;
+        }
+
+        friend const Timeval operator+(Timeval lhs, const Timeval &rhs)
+        { return lhs+=rhs; }
+
+        Timeval & operator-=(const Timeval &rhs)
+        {   
+            _M_tv.tv_sec -=  rhs.tv_sec();
+            _M_tv.tv_usec -= rhs.tv_usec();
+
+            if (_M_tv.tv_usec < 0) {
+                --_M_tv.tv_sec;
+                _M_tv.tv_usec += 1000000;
+            }
+            return *this;
+        }
+
+        friend const Timeval operator-(Timeval lhs, const Timeval &rhs)
+        { return lhs-=rhs; }
+
+        time_t
+        tv_sec() const
+        {
+            return _M_tv.tv_sec;
+        }
+    
+        suseconds_t
+        tv_usec() const
+        {
+            return _M_tv.tv_usec;
+        }
+
+        long long int 
+        to_sec() const 
+        { 
+            return static_cast<long long int>(_M_tv.tv_sec); 
+        }
+    
+        long long int 
+        to_msec() const 
+        { 
+            return static_cast<long long int>(_M_tv.tv_sec)*1000 + static_cast<long long int>(_M_tv.tv_usec)/1000; 
+        } 
+    
+        long long int 
+        to_usec() const 
+        { 
+            return static_cast<long long int>(_M_tv.tv_sec)*1000000 + static_cast<long long int>(_M_tv.tv_usec); 
+        }
+        
+        void 
+        update()
+        {
+            ::gettimeofday(&_M_tv,0);
+        }
+
+        /////////// conversion
+
+        operator bool_type() const
+        {
+            return (_M_tv.tv_sec || _M_tv.tv_usec) ? &Timeval::update : 0;
+        }
+
+        operator const timeval () const
+        {
+            return _M_tv;
+        }
+
+        operator const timespec () const
+        {
+            timespec ret;
+            ret.tv_sec  = _M_tv.tv_sec;
+            ret.tv_nsec = _M_tv.tv_usec * 1000; 
+            return ret;
+        }
+
+        /////////// static 
+
+        static Timeval now()
+        {
+            struct timeval now; ::gettimeofday(&now, 0);
+            return Timeval(now);
+        }
+
+    private:
+        struct timeval _M_tv;
+    };
+
+    static inline 
+    bool operator==(const Timeval &lhs, const Timeval &rhs)
+    {
+        return lhs.tv_sec() == rhs.tv_sec() && lhs.tv_usec() == rhs.tv_usec();
+    }
+    static inline
+    bool operator!=(const Timeval &lhs, const Timeval &rhs)
+    {
+        return !(lhs == rhs);
+    }
+    static inline
+    bool operator<(const Timeval &lhs, const Timeval &rhs)
+    {
+        return lhs.tv_sec() < rhs.tv_sec() || ( lhs.tv_sec() == rhs.tv_sec() && lhs.tv_usec() < rhs.tv_usec());
+    }    
+    static inline
+    bool operator>(const Timeval &lhs, const Timeval &rhs)
+    {
+        return lhs.tv_sec() > rhs.tv_sec() || ( lhs.tv_sec() == rhs.tv_sec() && lhs.tv_usec() > rhs.tv_usec());
+    }    
+    static inline
+    bool operator<=(const Timeval &lhs, const Timeval &rhs)
+    {
+        return !(lhs > rhs); 
+    }    
+    static inline
+    bool operator>=(const Timeval &lhs, const Timeval &rhs)
+    {
+        return !(lhs < rhs); 
+    }    
+
+    // global swap()
+    static inline 
+    void swap (Timeval& x, Timeval& y) 
+    { x.swap(y); }
+
+    template <typename CharT, typename Traits>
+    inline std::basic_ostream<CharT,Traits> &
+    operator<<(std::basic_ostream<CharT,Traits> &out, const more::Timeval &value)
+    {
+        out << "{ .sec=" << value.tv_sec() << 
+        " .usec=" << value.tv_usec() << " }";
+        return out;
+    }
+
+} // namespace more
+
+#endif /* _TIMEVAL_HPP_ */
