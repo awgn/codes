baf5a5b03ec8146008fecf479f7f84dc82488bd9
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2010-09-08 15:58:08 +0000

    minor change
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@555 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes.cpp0x/print-test.cpp
+++ trunk/codes.cpp0x/print-test.cpp
@@ -13,11 +13,25 @@
 #include <thread>
 #include <cstdio>
 
+struct tp
+{
+    void operator()() const
+    {
+        more::print(std::cout, "%1\n", std::this_thread::get_id());
+    }
+};
+
+
 int
 main(int argc, char *argv[])
 {
     more::print(std::cout, "%1 %2\n", "hello", std::string("world"));
     more::print(std::cout, "0x%1\n", more::flags<std::ios::hex>(3735928559));
+    more::print(std::cout, "%1\n", std::thread::id());
+
+    std::thread t((tp()));
+    t.join();
+
     return 0;
 }
  
