a6d52bb780466938d6647ca02fff9dac6fa9f4b3
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2011-01-02 12:29:39 +0000

    signal_slot: missing headers
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@606 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes.cpp0x/signal_slot.hpp
+++ trunk/codes.cpp0x/signal_slot.hpp
@@ -1,3 +1,13 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
 #ifndef _SIGNAL_SLOT_HPP_
 #define _SIGNAL_SLOT_HPP_ 
 
