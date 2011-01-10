ef89a8b55ecb762110838b027b12c8de61707844
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2011-01-10 17:20:37 +0000

    yats_run: minor fix
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@623 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes.rb/yats-run.rb
+++ trunk/codes.rb/yats-run.rb
@@ -1,13 +1,13 @@
 #!/usr/bin/ruby1.9.1 
 
 if __FILE__ == $0
-    executable = Dir.glob("test*").sort 
+    executable = Dir.glob("test*").delete_if { |e| !File.executable?(e) } 
     puts "YATS: running #{executable.size} executable tests..."
     n = 0
-    executable.each { |test|
+    executable.sort.each { |test|
         if(system("./#{test}"))
                 n += 1        
         end
     } 
-    puts "#{n}/#{executable.size} test passed."   
+    puts "#{n}/#{executable.size} tests passed."   
 end
