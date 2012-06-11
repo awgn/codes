42d9b20db4cec9bdaed4a771466228e6216c17e7
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2012-06-11 13:37:03 +0000

    Added iterator adapter for tuple value_type
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@753 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes.cpp11/iterator-test.cpp
@@ -0,0 +1,50 @@
+/* ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli 
+ * ----------------------------------------------------------------------------
+ */
+
+#include <map>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+
+#include <iterator.hpp>
+
+int
+main(int argc, char *argv[])
+{
+    typedef std::map<int, std::string> map_type;
+
+    map_type m { {1, "hello"}, {2, "world"}};
+
+    std::for_each(more::first_iterator(m.begin()),
+                  more::first_iterator(m.end()),
+                  [](int n) {
+
+                        std::cout << n << std::endl;
+                  });
+    
+    std::for_each(more::second_iterator(m.begin()),
+                  more::second_iterator(m.end()),
+                  [](std::string & n) {
+
+                        std::cout << n << std::endl;
+                  });
+
+    std::vector< std::tuple<int, std::string, bool> > v;
+
+    v.emplace_back(1, "hello", true);
+    v.emplace_back(2, "world", false);
+    
+    auto it = more::tuple_element_iterator<2>(v.begin());
+    
+    std::cout << std::boolalpha;
+    std::cout << it[0] << std::endl;
+    std::cout << it[1] << std::endl;
+    
+    return 0;
+}
+ 
---
+++ trunk/codes.cpp11/iterator.hpp
@@ -0,0 +1,170 @@
+/* ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli 
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef _ITERATOR_HPP_
+#define _ITERATOR_HPP_ 
+
+#include <iterator>
+#include <type_traits>
+#include <tuple>
+
+namespace more {
+
+    template <typename Iter, size_t N>
+    struct tuple_adapter_iterator
+    {
+        typedef typename Iter::iterator_category                        iterator_category;
+        typedef typename Iter::difference_type                          difference_type;
+        typedef typename std::tuple_element<N, typename Iter::value_type>::type  value_type;
+        typedef typename std::add_pointer<value_type>::type              pointer;
+        typedef typename std::add_lvalue_reference<value_type>::type    reference;
+
+        tuple_adapter_iterator(Iter it)
+        : it_(it)
+        {}
+
+        ~tuple_adapter_iterator()
+        {}
+
+        reference
+        operator *() const
+        {
+            return std::get<N>(*it_);
+        }
+
+        pointer
+        operator->() const
+        {
+            return &std::get<N>(*it_);
+        }
+
+        tuple_adapter_iterator&
+        operator++()
+        {
+            ++it_;
+            return *this;
+        }
+
+        tuple_adapter_iterator
+        operator++(int)
+        {
+            tuple_adapter_iterator ret(*this);
+            ++(*this);
+            return ret;
+        }
+
+        tuple_adapter_iterator&
+        operator--()
+        {
+            --it_;
+            return *this;
+        }
+
+        tuple_adapter_iterator
+        operator--(int)
+        {
+            tuple_adapter_iterator ret(*this);
+            --(*this);
+            return ret;
+        }
+
+        friend bool operator==(const tuple_adapter_iterator &lhs, const tuple_adapter_iterator &rhs)
+        {
+            return lhs.it_ == rhs.it_;
+        }
+
+        friend bool operator!=(const tuple_adapter_iterator &lhs, const tuple_adapter_iterator &rhs)
+        {
+            return lhs.it_ != rhs.it_;
+        }
+
+        // random access iterator requirements
+        //
+
+        reference
+        operator[](const difference_type &n) const
+        {
+            return std::get<N>(it_[n]);
+        }
+
+        tuple_adapter_iterator & 
+        operator+=(int n)
+        { 
+            it_ += n;
+            return *this;
+        }
+
+        friend const tuple_adapter_iterator operator+(tuple_adapter_iterator lhs, int n)
+        { return lhs+=n; }
+
+        tuple_adapter_iterator & 
+        operator-=(int n)
+        { 
+            it_ -= n;
+            return *this;
+        }
+
+        friend const tuple_adapter_iterator operator-(tuple_adapter_iterator lhs, int n)
+        { return lhs-=n; }
+
+        friend difference_type operator-(const tuple_adapter_iterator& lhs,
+                                         const tuple_adapter_iterator& rhs)
+        {
+            return lhs.it_-rhs.it_;
+        } 
+
+        friend bool operator<(const tuple_adapter_iterator& lhs,
+                              const tuple_adapter_iterator& rhs)
+        {
+            return lhs.it_ < rhs.it_;
+        }
+        friend bool operator> (const tuple_adapter_iterator& lhs,
+                               const tuple_adapter_iterator& rhs)
+        {
+            return rhs < lhs;
+        }
+        friend bool operator>=(const tuple_adapter_iterator& lhs,
+                               const tuple_adapter_iterator& rhs)
+        {
+            return !(lhs<rhs);
+        }
+        friend bool operator<=(const tuple_adapter_iterator& lhs,
+                               const tuple_adapter_iterator& rhs)
+        {
+            return !(lhs>rhs);
+        }
+    private:
+
+        Iter it_;
+    };
+
+    template <typename Iter>
+    auto first_iterator(Iter it)
+    -> tuple_adapter_iterator<Iter,0>
+    {
+        return tuple_adapter_iterator<Iter,0>(it);
+    }   
+
+    template <typename Iter>
+    auto second_iterator(Iter it)
+    -> tuple_adapter_iterator<Iter,1>
+    {
+        return tuple_adapter_iterator<Iter,1>(it);
+    }   
+
+    template <size_t N, typename Iter>
+    auto tuple_element_iterator(Iter it)
+    -> tuple_adapter_iterator<Iter, N>
+    {
+        return tuple_adapter_iterator<Iter,N>(it);
+    }   
+
+} // namespace more
+
+
+#endif /* _ITERATOR_HPP_ */
