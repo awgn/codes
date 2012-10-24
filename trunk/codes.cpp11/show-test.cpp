e0e97aebf36c4d6c0fbfbaf962d479b394734fc9
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2012-10-24 21:37:42 +0000

    Added show.hpp that deprecates streamer.hpp
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@792 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes.cpp11/show-test.cpp
@@ -0,0 +1,97 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
+#include <show.hpp> // !more
+
+#include <vector>
+#include <list>
+#include <map>
+#include <iostream>
+
+struct test
+{
+    int value;
+};
+
+inline std::string
+show(const test &t)
+{
+    return "test " + std::to_string(t.value);    
+}
+
+
+int 
+main(int, char *[])
+{
+    std::cout << "--- basic (only values):\n\n";
+
+    std::cout << show("hello world") << std::endl; 
+    std::cout << show(std::string("hello world")) << std::endl; 
+
+    std::cout << show((void *)0xdeadbeef) << std::endl; 
+
+    auto p = std::make_pair("hello world", 42);
+
+    std::cout << show(p) << std::endl; 
+    
+    std::vector<int> v1;
+
+    v1.push_back(0);
+    v1.push_back(1);
+    v1.push_back(2);
+
+    std::cout << show(v1) << std::endl;
+
+    std::vector<char> v2;
+
+    v2.push_back('a');
+    v2.push_back('b');
+    v2.push_back('c');
+
+    std::cout << show(v2) << std::endl;
+
+    std::list<std::pair<int,int> > l1;
+
+    l1.push_back( std::make_pair(0,0) );
+    l1.push_back( std::make_pair(1,1) );
+    l1.push_back( std::make_pair(2,2) );
+
+    std::cout << show(l1) << std::endl;
+
+    std::map<std::string,int> m;
+
+    m["hello"]=0;
+    m["world"]=1;
+
+    std::cout << show(m) << std::endl;
+
+    std::cout << show(std::tuple<int,double>(1,1.1)) << std::endl;
+    std::cout << show(std::array<int,3>()) << std::endl;
+
+    std::cout << show(std::chrono::system_clock::now()) << std::endl;
+    std::cout << show(std::chrono::milliseconds(10)) << std::endl;
+    std::cout << show(test()) << std::endl;
+
+    std::cout << "\n--- with typename:\n\n";
+
+    auto c = std::make_tuple(std::vector<int>{1,2}, std::make_pair(1,2), std::chrono::seconds(1), std::make_tuple(1,2));
+    std::cout << show(c, "") << std::endl;
+
+    std::cout << "\n--- with custom name:\n\n";
+
+    std::cout << show(42, "Integer:") << std::endl;
+    
+    int c_array[3] = {1,2,3};
+    
+    std::cout << show(c_array, "") << std::endl;
+    std::cout << show(c_array, "C array") << std::endl;
+
+    return 0;
+}
---
+++ trunk/codes.cpp11/show.hpp
@@ -0,0 +1,253 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
+#ifndef _SHOW_HPP_
+#define _SHOW_HPP_
+
+#include <type_traits.hpp>  // more!
+
+#include <type_traits>
+#include <array>
+#include <tuple>
+#include <chrono>
+#include <memory>
+
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+#include <string>
+#include <sstream>
+
+#include <cxxabi.h>
+
+
+inline namespace more_show {
+
+    // forward declarations:
+    //
+    
+    inline std::string 
+    show(const char *v, const char *n = nullptr);
+    
+    inline std::string 
+    show(std::string const &s, const char *n = nullptr);
+
+    template <typename T> 
+    inline 
+    typename std::enable_if<std::is_arithmetic<T>::value, std::string>::type 
+    show(T const &value, const char * n = nullptr);
+
+    template <typename T>
+    inline  
+    typename std::enable_if<std::is_pointer<T>::value, std::string>::type 
+    show(T const &p, const char *n = nullptr);
+
+    template <typename U, typename V>
+    inline std::string
+    show(std::pair<U,V> const &r, const char * n = nullptr);
+
+    template <typename T, std::size_t N>
+    inline std::string
+    show(std::array<T,N> const &a, const char * n = nullptr);
+
+    template <typename ...Ts>
+    inline std::string
+    show(std::tuple<Ts...> const &t, const char * n = nullptr);
+
+    template <typename Rep, typename Period>
+    inline std::string
+    show(std::chrono::duration<Rep, Period> const &dur, const char *n = nullptr);
+    
+    template <typename Clock, typename Dur>
+    inline std::string
+    show(std::chrono::time_point<Clock, Dur> const &r, const char *n = nullptr);
+
+    template <typename T>
+    inline typename std::enable_if<
+        (!std::is_pointer<T>::value) && (
+        (more::traits::is_container<T>::value && !std::is_same<typename std::string,T>::value) ||
+        (std::rank<T>::value > 0 && !std::is_same<char, typename std::remove_cv<typename std::remove_all_extents<T>::type>::type>::value)),
+    std::string>::type 
+    show(const T &v, const char * n = nullptr);
+    
+    namespace details {
+
+        inline std::string
+        cxa_demangle(const char *name)
+        {
+            int status;
+            std::unique_ptr<char, void(*)(void *)> ret(abi::__cxa_demangle(name,0,0, &status), ::free);
+            if (status < 0) {
+                return std::string("?");
+            }
+            return std::string(ret.get());
+        }
+
+        template <typename T>
+        inline std::string 
+        header(const char *n)
+        {
+            return n == nullptr ? std::string() : 
+                    *n == '\0' ? cxa_demangle(typeid(T).name()) + " " :
+                     std::string(n) + " ";
+        }
+
+        // show_on policy 
+        //
+
+        template <typename T, int N>
+        struct show_on
+        {
+            static void apply(std::string &out, const T &tupl, const char *n)
+            {
+                out += show(std::get< std::tuple_size<T>::value - N>(tupl), nullptr) + " ";
+                show_on<T,N-1>::apply(out,tupl, n);
+            }
+        }; 
+        template <typename T>
+        struct show_on<T, 0>
+        {
+            static void apply(std::string&, const T &, const char *)
+            {}
+        };
+
+        template <typename T>
+        struct _duration_traits;
+            template <> struct _duration_traits<std::chrono::nanoseconds>  { static constexpr const char *str = "_ns"; };
+            template <> struct _duration_traits<std::chrono::microseconds> { static constexpr const char *str = "_us"; };
+            template <> struct _duration_traits<std::chrono::milliseconds> { static constexpr const char *str = "_ms"; };
+            template <> struct _duration_traits<std::chrono::seconds>      { static constexpr const char *str = "_s"; };
+            template <> struct _duration_traits<std::chrono::minutes>      { static constexpr const char *str = "_m"; };
+            template <> struct _duration_traits<std::chrono::hours>        { static constexpr const char *str = "_h"; };
+
+    }
+
+    ///////////////////////////////////////
+    // show for const char *
+    //
+    
+    inline std::string
+    show(const char *v, const char *n)
+    {
+        return details::header<const char *>(n) + "\"" + std::string(v) + "\"";
+    }
+    
+    ///////////////////////////////////////
+    // show for std::string
+    //
+    
+    inline std::string
+    show(std::string const &s, const char *n)
+    {
+        return details::header<std::string>(n) + "\"" + s + "\"";
+    }
+
+    ///////////////////////////////////////
+    // show for arithmetic types..
+    //
+    
+    template <typename T>
+    inline typename std::enable_if<std::is_arithmetic<T>::value, std::string>::type
+    show(T const &value, const char * n)
+    {
+        return details::header<T>(n) + std::to_string(value);
+    }
+
+    ///////////////////////////////////////
+    // show for pointers *
+    //
+    
+    template <typename T> 
+    inline typename std::enable_if<std::is_pointer<T>::value, std::string>::type
+    show(T const &p, const char *n)
+    {
+        std::ostringstream o;
+        o << static_cast<void *>(p);
+        return details::header<T>(n) + o.str();
+    }
+
+    //////////////////////////
+    // show for pair...
+
+    template <typename U, typename V>
+    inline std::string
+    show(const std::pair<U,V> &r, const char * n)
+    {
+        return details::header<std::pair<U,V>>(n) + 
+                "(" + show(r.first) + 
+                "," + show(r.second) + ")";
+    }
+
+    ///////////////////////////
+    // show for array...
+
+    template <typename T, std::size_t N>
+    inline std::string
+    show(std::array<T,N> const &a, const char * n)
+    {
+        std::string out("[ ");
+        details::show_on<std::array<T,N>, N>::apply(out,a, n ? "" : nullptr);
+        return details::header<std::array<T,N>>(n) + out + "]";
+    }
+
+    ////////////////////////////////////////////////////////
+    // show for tuple... 
+
+    template <typename ...Ts>
+    inline std::string
+    show(std::tuple<Ts...> const &t, const char * n)
+    {
+        std::string out("{ ");
+        details::show_on<std::tuple<Ts...>, sizeof...(Ts)>::apply(out,t, n ? "" : nullptr);
+        return details::header<std::tuple<Ts...>>(n) + out + "}";
+    }                                              
+
+    ////////////////////////////////////////////////////////
+    // show for chrono types... 
+
+    template <typename Rep, typename Period>
+    inline std::string
+    show(std::chrono::duration<Rep, Period> const &dur, const char *n)
+    {
+        std::string out(std::to_string(dur.count()));
+        return details::header<std::chrono::duration<Rep,Period>>(n) + out + std::string(details::_duration_traits<std::chrono::duration<Rep,Period>>::str);
+    }
+
+    template <typename Clock, typename Dur>
+    inline std::string
+    show(std::chrono::time_point<Clock, Dur> const &r, const char *n)
+    {    
+        return details::header<std::chrono::time_point<Clock,Dur>>(n) + show(r.time_since_epoch());
+    }
+
+    ///////////////////////////////////////
+    // show for generic containers...
+    //
+
+    template <typename T>
+    inline typename std::enable_if<
+        (!std::is_pointer<T>::value) && (
+        (more::traits::is_container<T>::value && !std::is_same<typename std::string,T>::value) ||
+        (std::rank<T>::value > 0 && !std::is_same<char, typename std::remove_cv<typename std::remove_all_extents<T>::type>::type>::value)),
+    std::string>::type 
+    show(const T &v, const char * n)
+    {
+        std::string s("{ ");
+        for(auto & e : v)
+        {
+            s += show(e) + " ";
+        }
+        return details::header<T>(n) + s + "}";
+    };
+
+} // namespace more_show
+
+
+#endif /* _SHOW_HPP_ */
