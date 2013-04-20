da267caeb0a6af53b8c13a85223967bd32821bb5
nicola.bonelli <nicola.bonelli@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2013-04-20 08:07:26 +0000

    Added persistent header
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@853 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

---
+++ trunk/codes.cpp11/persistent.hpp
@@ -0,0 +1,117 @@
+/* $Id$ */
+/*
+ * ----------------------------------------------------------------------------
+ * "THE BEER-WARE LICENSE" (Revision 42):
+ * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
+ * can do whatever you want with this stuff. If we meet some day, and you think
+ * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli
+ * ----------------------------------------------------------------------------
+ */
+
+#pragma once 
+
+#include <fstream>
+#include <type_traits>
+#include <stdexcept>
+#include <memory>
+
+namespace more {
+
+    template <typename Tp>
+    struct persistent
+    {
+        static_assert(std::is_pod<Tp>::value, "persistent Tp must be a pod");
+
+        persistent(const char *name)
+        : stream_(new std::fstream(name, std::ios::in|std::ios::out|std::ios::binary))
+        {
+            if (!*stream_)
+            {
+                value_initialize_(name);
+                
+                stream_->open(name, std::ios::in|std::ios::out|std::ios::binary);
+                if (!*stream_)
+                    throw std::runtime_error("persistent::persistent");
+            }
+            else
+            {
+                stream_->read(reinterpret_cast<char *>(&value_), sizeof(Tp));
+                if (!*stream_)
+                    throw std::runtime_error("persistent::persistent");
+            }
+        }
+
+        persistent(std::string const &name)
+        : persistent(name.c_str())
+        {}
+
+        persistent(const char *name, Tp const &value)
+        : stream_(name, std::ios::trunc|std::ios::out|std::ios::binary) 
+        , value_(value)
+        {
+            store_();
+        }
+        
+        persistent(std::string const &name, Tp const &value)
+        : persistent(name.c_str(), value)
+        {}
+
+        persistent(const persistent &) = delete;
+        persistent& operator=(const persistent &) = delete;
+
+
+        persistent(persistent &&other)
+        : stream_(std::move(other.stream_))
+        , value_(std::move(other.value_))
+        {
+        }
+
+        persistent& operator=(persistent &&other)
+        {
+            if (this != &other)
+            {
+                stream_ = std::move(other.stream_);
+                value_  = std::move(other.value_);
+            }
+            return *this;
+        }
+
+        const Tp &
+        get() const
+        {
+            return value_;
+        }
+
+        template <typename F>
+        void update(F fun)
+        {
+            fun(value_);
+            store_();
+        }
+    
+
+    private:               
+
+        void value_initialize_(const char *name)
+        {
+            stream_->open(name, std::ios::trunc|std::ios::out|std::ios::binary);
+            value_ = Tp();
+            store_();
+            stream_->close();
+        }
+
+        void store_()
+        {
+            stream_->seekg(0);
+            stream_->write(reinterpret_cast<const char *>(&value_), sizeof(Tp));
+            if (!*stream_)
+                throw std::runtime_error("persistent::store");
+        }
+
+        std::unique_ptr<std::fstream> stream_;
+
+        mutable Tp value_;
+    };
+
+
+} // namespace more
