ba2bd695d5197f6881e5a191fe2e372fc0d541eb
nicola.bonelli@gmail.com <nicola.bonelli@gmail.com@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2014-01-09 12:57:14 +0000

    MAJOR: ***KEY_VALUE PARSER REWRITE (functional design) ***
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@929 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes.cpp11/key_value-test.cpp
+++ trunk/codes.cpp11/key_value-test.cpp
@@ -1,146 +1,119 @@
-/* $Id$ */
-/*
- * ----------------------------------------------------------------------------
- * "THE BEER-WARE LICENSE" (Revision 42):
- * <bonelli@antifork.org> wrote this file. As long as you retain this notice you
- * can do whatever you want with this stuff. If we meet some day, and you think
- * this stuff is worth it, you can buy me a beer in return. Nicola Bonelli 
- * ----------------------------------------------------------------------------
- */
-
-#include <iostream>
-#include <algorithm>
-#include <iterator>
-#include <cstdlib>
-#include <vector>
-#include <list>
-#include <map>
-#include <set>
-
+#include <variant.hpp>
+#include <show.hpp>
 #include <key_value.hpp>
 
-MAP_KEY_VALUE(unsigned int, unsigned_int, 3)   // default value for the key  
+#include <list>
+#include <string>
+#include <set>
+#include <map>
+#include <memory>
 
-MAP_KEY(std::vector<int>, integers)
+namespace key
+{
+    DECLARE_KEY(unsigned_int);
+    DECLARE_KEY(integers);
+    DECLARE_KEY(booleans);
+    DECLARE_KEY(strings);
+    DECLARE_KEY(intset);
 
-MAP_KEY(std::vector<bool>, booleans)
-MAP_KEY(std::list<std::string>, strings)
+    DECLARE_KEY(associative);
+    DECLARE_KEY(simple_pair);
+    DECLARE_KEY(simple_tuple);
 
-MAP_KEY(std::set<int>, intset)
+    DECLARE_KEY(ptr_int);
+    DECLARE_KEY(shared_int);
+    DECLARE_KEY(unique_int);
 
-typedef std::map<std::string,int> map_type;
-MAP_KEY(map_type, associative)
- 
-typedef std::pair<double,double> pair_type;  
-MAP_KEY(pair_type, simple_pair)
+    DECLARE_KEY(lit_string);
+    DECLARE_KEY(int_matrix);
 
-typedef std::tuple<bool, int, double, std::string> tuple_type;
-MAP_KEY(tuple_type, simple_tuple)
+    DECLARE_KEY(block);
 
-namespace simple_block {
+    DECLARE_KEY(int_first);
+    DECLARE_KEY(int_second);
 
-    MAP_KEY(int, first)
-    MAP_KEY(int, second)
-
-    typedef more::key_value_pack<first, second>  type;
+    DECLARE_KEY(v_int_string);
 }
 
-typedef std::vector<std::pair<simple_block::type,bool>> block_vec;
+using namespace more::key_value;
 
-MAP_KEY(block_vec, block) 
+typedef parser<
 
-// smart pointer support
+    options<true, '#', '='>,
 
-MAP_KEY(double *, raw_double)
-MAP_KEY(std::shared_ptr<int>, shared_int)
+    pair<key::int_first,  int>,
+    pair<key::int_second, int>
 
-MAP_KEY(const char *, literal)
+   > int_pair;
 
-MAP_KEY(std::vector<std::vector<int>>, matrix)
 
-typedef more::key_value_pack<unsigned_int,
-                               integers,
-                               booleans,
-                               strings, 
-                               intset,
-                               associative,
-                               simple_pair,
-                               simple_tuple,
-                               block,
-                               raw_double,
-                               shared_int,
-                               literal,
-                               matrix> my_config;
+typedef parser<
+
+    options<false, '#', '='>,
+
+    pair<key::unsigned_int, int>,
+    pair<key::integers,     std::vector<int>>,
+    pair<key::booleans,     std::vector<bool>>,
+    pair<key::strings,      std::list<std::string>>,
+    pair<key::simple_pair,  std::pair<double, double>>,
+    pair<key::simple_tuple, std::tuple<bool, int, double, std::string>>,
+    pair<key::associative,  std::map<std::string, int>>,
+    pair<key::intset,       std::set<int>>,
+    pair<key::ptr_int,      int *>,
+    pair<key::shared_int,   std::shared_ptr<int>>,
+    pair<key::unique_int,   std::unique_ptr<int>>,
+    pair<key::lit_string,   const char *>,
+    pair<key::int_matrix,   std::vector<std::vector<int>>>,
+    pair<key::block,        std::vector<int_pair>>,
+    pair<key::v_int_string, more::variant<int, std::string>>
+
+    > this_parser;
+
+
+template <typename K, typename Par>
+void show_value(Par const &par)
+    {
+        std::cout << "-> " << K::str() << " = " << show (par.template get<K>()) << std::endl;
+    }
+
+template <typename K, typename Par>
+void show_pointer(Par const &par)
+    {
+        auto &p = par.template get<K>();
+
+        std::cout << "-> " << K::str() << " = ";
+        if (p)
+            std::cout << show (*p) << '(' << show(p) << ')'<< std::endl;
+        else 
+            std::cout << "nullptr" << std::endl;
+    }
 
 int
-main(int, char *[])
+main(int argc, char *argv[])
 {
-    const my_config conf("key_value_test.txt", more::key_value_opt::non_strict());
+    this_parser par;
 
-    // or...
-    // if (!conf.load("key_value_test.txt") ) {
-    //     return -1;
-    // }
+    if (!parse("key_value_test.txt", par))
+        exit(1);
 
-    std::cout << "-> " << unsigned_int::str() << " = " << more::get<unsigned_int>(conf) << std::endl;
+    show_value<key::unsigned_int>(par);
+    show_value<key::integers>(par);
+    show_value<key::booleans>(par);
+    show_value<key::strings>(par);
+    show_value<key::simple_pair>(par);
+    show_value<key::simple_tuple>(par);
+    show_value<key::associative>(par);
+    show_value<key::intset>(par);
+    show_value<key::lit_string>(par);
+    show_value<key::int_matrix>(par);
 
-    std::cout << "-> " << integers::str() << " = ";
-    std::copy(conf.get<integers>().begin(), conf.get<integers>().end(), std::ostream_iterator<int>(std::cout," "));
-    std::cout << std::endl;
+    show_value<key::v_int_string>(par);
 
-    std::cout << "-> " << booleans::str() << " = ";
-    std::copy(conf.get<booleans>().begin(), conf.get<booleans>().end(), std::ostream_iterator<int>(std::cout," "));
-    std::cout << std::endl;
+    show_value<key::block>(par);
 
-    std::cout << "-> " << strings::str() << " = ";
-    std::copy(conf.get<strings>().begin(), conf.get<strings>().end(), std::ostream_iterator<std::string>(std::cout,"|"));
-    std::cout << std::endl;
-
-    std::cout << "-> " << associative::str() << " elem:" << conf.get<associative>().size() << std::endl;
-    auto it = conf.get<associative>().begin();
-    auto it_end = conf.get<associative>().end();
-    for(; it != it_end; ++it)
-    {
-        std::cout << "   key:" << (*it).first << " -> value:" << (*it).second << std::endl; 
-    }
-
-    std::cout << "-> " << intset::str() << " elem:" << conf.get<intset>().size() << std::endl;
-    auto sit = conf.get<intset>().begin();
-    auto sit_end = conf.get<intset>().end();
-    for(; sit != sit_end; ++sit)
-    {
-        std::cout << "   value:" << (*sit) << std::endl; 
-    }
-
-    std::cout << "-> " << simple_pair::str() << " = (" << conf.get<simple_pair>().first << " , " << conf.get<simple_pair>().second << ")" << std::endl;
-
-    std::cout << "-> " << simple_tuple::str() << " = (" << std::get<0>( more::get<simple_tuple>(conf) ) << " , " <<
-    std::get<1>( more::get<simple_tuple>(conf) ) << " , " <<
-    std::get<2>( more::get<simple_tuple>(conf) ) << " , " <<
-    std::get<3>( more::get<simple_tuple>(conf) ) << ")" << std::endl;
-
-    // std::cout << "   block -> first:  " << more::get<simple_block::first>(more::get<block>(conf)) << '\n' <<
-    // "            second: " << more::get<simple_block::second>(more::get<block>(conf)) << std::endl;
-
-    std::cout << " -> " << raw_double::str() << " => " << std::boolalpha << static_cast<bool>(more::get<raw_double>(conf));
-    if (more::get<raw_double>(conf))
-        std::cout << " value:" << *more::get<raw_double>(conf);
-    std::cout << std::endl;
-
-    std::cout << " -> " << shared_int::str() << " => " << std::boolalpha << static_cast<bool>(more::get<shared_int>(conf));
-    if( more::get<shared_int>(conf) )
-        std::cout << " value:" << *more::get<shared_int>(conf);
-    std::cout << std::endl;
-
-    std::cout << " -> " << literal::str() << " => " << std::boolalpha << static_cast<bool>(more::get<literal>(conf));
-    if( more::get<literal>(conf) )
-        std::cout << " value:" << more::get<literal>(conf);
-    std::cout << std::endl;
-
-    std::cout << " -> " << matrix::str() << " =\n";
-    auto m = more::get<matrix>(conf);
-    std::cout << "     " << m[0][0] << ' ' << m[0][1] << std::endl;
-    std::cout << "     " << m[1][0] << ' ' << m[1][1] << std::endl;
+    show_pointer<key::ptr_int>(par);
+    show_pointer<key::shared_int>(par);
+    show_pointer<key::unique_int>(par);
 
     return 0;
 }
