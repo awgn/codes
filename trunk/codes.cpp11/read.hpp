62e8ceb56a3961348e923a725763ee1e225e54c9
nicola.bonelli@gmail.com <nicola.bonelli@gmail.com@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2014-01-09 10:20:34 +0000

    Using cxxabi.hpp library.
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@917 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes.cpp11/read.hpp
+++ trunk/codes.cpp11/read.hpp
@@ -22,7 +22,7 @@
 #include <iostream>
 
 #include <type_traits.hpp>
-#include <cxxabi.h>
+#include <cxxabi.hpp>
 
 inline namespace more_read {
 
@@ -90,17 +90,6 @@ inline namespace more_read {
         {
             typedef std::tuple<typename std::decay<Ts>::type...> type;
         };
-
-        inline std::string
-        demangle(const char * name)
-        {
-            int status;
-            std::unique_ptr<char, void(*)(void *)> ret(abi::__cxa_demangle(name,0,0, &status), ::free);
-            if (status < 0) {
-                return std::string(1,'?');
-            }
-            return std::string(ret.get());
-        }
 
         template <typename Tp>
         std::string
