48a4a4b08126b4716aad526d3a55d6f3db745ef9
nicola.bonelli@gmail.com <nicola.bonelli@gmail.com@0daf1152-ea34-11dd-b00f-9fabb4b2ff13>
2014-01-10 13:57:14 +0000

    Added byte-string test.
    git-svn-id: https://nicola-bonelli-repo.googlecode.com/svn@947 0daf1152-ea34-11dd-b00f-9fabb4b2ff13

--- trunk/codes.cpp11/byte_string-test.cpp
+++ trunk/codes.cpp11/byte_string-test.cpp
@@ -8,15 +8,105 @@
  * ----------------------------------------------------------------------------
  */
  
+#include "yats.hpp"
 #include "byte_string.hpp"
 
-int
-main(int argc, char *argv[])
-{
-    more::byte_string<6> a("hello");
-    more::byte_string<6> b("world");
-    std::cout << (a < b) << std::endl;
+using namespace yats;
+using namespace more;
 
-    return 0;
+Context(ByteString)
+{
+    Test(ctor)
+    {
+        byte_string<8> x;
+        Assert( x.empty(), is_true());
+        Assert( static_cast<bool>(x), is_false());
+
+        Assert( x.length(), is_equal_to(0));
+        Assert( x.size  (), is_equal_to(0));
+        Assert( x.capacity(), is_equal_to(7));
+    }
+
+    Test(ctor2)
+    {
+        byte_string<8> x("hello");
+        Assert( x.empty(), is_false());
+        Assert( static_cast<bool>(x), is_true());
+        Assert( x.length(), is_equal_to(5));
+        Assert( x.size  (), is_equal_to(5));
+        Assert( x.capacity(), is_equal_to(7));
+    }
+    
+    Test(ctor3)
+    {
+        byte_string<8> x(std::string("hello"));
+        Assert( x.empty(), is_false());
+    }
+
+    Test(copyctor)
+    {
+        byte_string<8> x("hello");
+        byte_string<6> y(x);
+
+        AssertThrow( byte_string<5> z(y) );
+        Assert(x == y);
+    }
+
+    Test(assign)
+    {
+        byte_string<8> x("hello");
+        byte_string<6> y;
+        byte_string<4> z;
+                             
+        AssertNothrow  ( y = x );
+        AssertThrow    ( z = x );
+
+        Assert(x == y);
+    }
+
+    Test(operEq)
+    {
+        byte_string<8> x("hello");
+        byte_string<6> y(x);
+
+        Assert(x == x);
+        Assert(x == y);
+        Assert(y == x);
+        
+        Assert(x == "hello");
+        Assert("hello" == x);
+        Assert(x == std::string("hello"));
+        Assert(std::string("hello") == x);
+    }
+
+    Test(operNotEq)
+    {
+        byte_string<8> x("hello");
+        byte_string<6> y(x);
+
+        Assert(!(x != x));
+        Assert(!(x != y));
+        Assert(!(y != x));
+        
+        Assert(!(x != "hello"));
+        Assert(!("hello" != x));
+        Assert(!(x != std::string("hello")));
+        Assert(!(std::string("hello") != x));
+    }
+    
+    Test(operLess)
+    {
+        byte_string<8> x("hello");
+        byte_string<6> y("world");
+
+        Assert( x < y );
+    }
+}
+
+
+int
+main(int argc, char*  argv[])
+{
+    return yats::run(argc, argv);
 }
 
