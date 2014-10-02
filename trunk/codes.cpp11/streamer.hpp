939ce54db8012121c73519305edeab8c1000b800
Nicola Bonelli <nicola.bonelli@gmail.com>
2014-10-02 10:21:44 +0200

    Minor changes

--- trunk/codes.cpp11/streamer.hpp
+++ trunk/codes.cpp11/streamer.hpp
@@ -46,12 +46,12 @@ namespace more { namespace streamer {
 
         template <typename T>
         struct _duration_traits;
-            template <> struct _duration_traits<std::chrono::nanoseconds> { static constexpr const char  *str = "_ns"; };
+            template <> struct _duration_traits<std::chrono::nanoseconds>  { static constexpr const char *str = "_ns"; };
             template <> struct _duration_traits<std::chrono::microseconds> { static constexpr const char *str = "_us"; };
             template <> struct _duration_traits<std::chrono::milliseconds> { static constexpr const char *str = "_ms"; };
-            template <> struct _duration_traits<std::chrono::seconds> { static constexpr const char *str = "_s"; };
-            template <> struct _duration_traits<std::chrono::minutes> { static constexpr const char *str = "_m"; };
-            template <> struct _duration_traits<std::chrono::hours> { static constexpr const char *str = "_h"; };
+            template <> struct _duration_traits<std::chrono::seconds>      { static constexpr const char *str = "_s"; };
+            template <> struct _duration_traits<std::chrono::minutes>      { static constexpr const char *str = "_m"; };
+            template <> struct _duration_traits<std::chrono::hours>        { static constexpr const char *str = "_h"; };
 
 } // namespace streamer
 } // namespace more
@@ -65,10 +65,8 @@ namespace std {
 
     template <typename CharT, typename Traits, typename T>
     inline typename std::enable_if<
-        (more::traits::is_container<T>::value &&
-            !is_same<typename std::string,T>::value) ||
-        (rank<T>::value > 0 &&
-            !is_same<char, typename remove_cv<typename remove_all_extents<T>::type>::type>::value),
+        (more::traits::is_container<T>::value && !is_same<typename std::string,T>::value) ||
+        (rank<T>::value > 0 && !is_same<char, typename remove_cv<typename remove_all_extents<T>::type>::type>::value),
         std::basic_ostream<CharT,Traits>>::type &
     operator<<(std::basic_ostream<CharT,Traits> &out, const T &v)
     {
@@ -102,27 +100,27 @@ namespace std {
         return out << ']';
     }
 
-    ////////////////////////////////////////////////////////
+    ///////////////////////////////////////////////////////
     // operator<< for tuple: (enabled if T is a tuple<>)...
 
     template <typename CharT, typename Traits, typename T>
     typename std::enable_if< more::traits::is_tuple<T>::value, std::basic_ostream<CharT,Traits> >::type &
     operator<<(std::basic_ostream<CharT,Traits> &out, const T &rhs)
     {
-        out << "{ ";
+        out << "( ";
         more::streamer::printon<CharT, Traits, T, std::tuple_size<T>::value>::apply(out,rhs);
-        return out << '}';
+        return out << ')';
     }
 
     ////////////////////////////////////////////////////////
     // operator<< for chrono types...
 
     template <typename CharT, typename Traits, typename Tp>
-    typename std::enable_if< std::is_same<Tp, std::chrono::nanoseconds>::value ||
+    typename std::enable_if< std::is_same<Tp, std::chrono::nanoseconds>::value  ||
                              std::is_same<Tp, std::chrono::microseconds>::value ||
                              std::is_same<Tp, std::chrono::milliseconds>::value ||
-                             std::is_same<Tp, std::chrono::seconds>::value ||
-                             std::is_same<Tp, std::chrono::minutes>::value ||
+                             std::is_same<Tp, std::chrono::seconds>::value      ||
+                             std::is_same<Tp, std::chrono::minutes>::value      ||
                              std::is_same<Tp, std::chrono::hours>::value,
              std::basic_ostream<CharT, Traits>>::type &
     operator<< (std::basic_ostream<CharT, Traits> &out, const Tp &r)
